268d62a4a01166c853011da83e3372d9ab93554e
Maximilien Noal <noal.maximilien@gmail.com>
2021-12-27 17:45:56 +0100

    Init GUI, added ErrorProne analyzer
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Ix86.View/App.axaml.cs
+++ src/Ix86.View/App.axaml.cs
@@ -3,27 +3,113 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Ix86.View.ViewModels;
 using Ix86.View.Views;
+using System;
+using Microsoft.Win32;
+using System.Runtime.Versioning;
+using ReactiveUI;
+using Live.Avalonia;
+using System.Reactive;
+using System.Diagnostics;
+using Avalonia.Controls;
 
 namespace Ix86.View
 {
-    public class App : Application
+    public class App : Application, ILiveView
     {
-        public override void Initialize()
+        private const string RegistryKeyPath = @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+
+        private const string RegistryValueName = "AppsUseLightTheme";
+
+        public override void Initialize() => AvaloniaXamlLoader.Load(this);
+
+        private static bool IsProduction()
         {
-            AvaloniaXamlLoader.Load(this);
+#if DEBUG
+            return false;
+#else
+        return true;
+#endif
         }
+
+        // When any of the source files change, a new version of the assembly is
+        // built, and this method gets called. The returned content gets embedded
+        // into the LiveViewHost window.
+        public object CreateView(Window window) => new MainWindow();
 
         public override void OnFrameworkInitializationCompleted()
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow
+                if (IsInDarkMode() == false)
                 {
-                    DataContext = new MainWindowViewModel(),
-                };
+                    this.Styles.RemoveAt(1);
+                }
+
+                if (Debugger.IsAttached || IsProduction())
+                {
+                    // Debugging requires pdb loading etc, so we disable live reloading
+                    // during a test run with an attached debugger.
+                    var mainWindow = new MainWindow();
+                    mainWindow.DataContext = MainWindowViewModel.Create(mainWindow);
+                    mainWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                    desktop.MainWindow = mainWindow;
+                    mainWindow.Show();
+                }
+                else
+                {
+                    // Here, we create a new LiveViewHost, located in the 'Live.Avalonia'
+                    // namespace, and pass an ILiveView implementation to it. The ILiveView
+                    // implementation should have a parameterless constructor! Next, we
+                    // start listening for any changes in the source files. And then, we
+                    // show the LiveViewHost window. Simple enough, huh?
+                    var window = new LiveViewHost(this, Console.WriteLine);
+                    window.StartWatchingSourceFilesForHotReloading();
+                    window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                    window.Show();
+                }
+
+                // Here we subscribe to ReactiveUI default exception handler to avoid app
+                // termination in case if we do something wrong in our view models. See:
+                // https://www.reactiveui.net/docs/handbook/default-exception-handler/
+                //
+                // In case if you are using another MV* framework, please refer to its
+                // documentation explaining global exception handling.
+                RxApp.DefaultExceptionHandler = Observer.Create<Exception>(Console.WriteLine);
+
+                base.OnFrameworkInitializationCompleted();
+            }
+        }
+
+        [SupportedOSPlatform("windows")]
+        private static bool GetIsWindowsInDarkMode()
+        {
+            var key = Registry.CurrentUser.OpenSubKey(RegistryKeyPath);
+            var registryValueObject = key?.GetValue(RegistryValueName);
+            if (registryValueObject == null)
+            {
+                return false;
             }
 
-            base.OnFrameworkInitializationCompleted();
+            var registryValue = (int)registryValueObject;
+            return registryValue <= 0;
+        }
+
+        private bool IsInDarkMode()
+        {
+#pragma warning disable ERP022 // Unobserved exception in generic exception handler
+            try
+            {
+                if (OperatingSystem.IsWindows())
+                {
+                    return GetIsWindowsInDarkMode();
+                }
+            }
+            catch
+            {
+                //No OS support for themes. Not worth crashing for. Not worth reporting.
+            }
+#pragma warning restore ERP022 // Unobserved exception in generic exception handler
+            return false;
         }
     }
 }
--- src/Ix86.View/ViewModels/MainWindowViewModel.cs
+++ src/Ix86.View/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ix86.View.Views;
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +8,18 @@ namespace Ix86.View.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private MainWindow _mainWindow;
+
+        public MainWindowViewModel(MainWindow window)
+        {
+            _mainWindow = window;
+        }
+
         public string Greeting => "Welcome to Avalonia!";
+
+        internal static MainWindowViewModel Create(MainWindow mainWindow)
+        {
+            return new MainWindowViewModel(mainWindow);
+        }
     }
 }
