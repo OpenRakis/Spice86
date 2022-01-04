1fcf7fbd33253210fba725fceb635e959098ce6d
Maximilien Noal <noal.maximilien@gmail.com>
2022-01-04 23:27:16 +0100

    More translation from Spice86: ExeFileLoader
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ src/Spice86/Emulator/LoadableFile/ExecutableFileLoader.cs
@@ -0,0 +1,47 @@
+﻿namespace Spice86.Emulator.LoadableFile;
+
+using Serilog;
+
+using Spice86.Emulator.Cpu;
+using Spice86.Emulator.Machine;
+using Spice86.Emulator.Memory;
+using Spice86.Utils;
+
+using System.IO;
+
+/// <summary>
+/// Base class for loading executable files in the VM like exe, bios, ...
+/// </summary>
+public abstract class ExecutableFileLoader
+{
+    private static readonly ILogger _logger = Log.Logger.ForContext<ExecutableFileLoader>();
+
+    protected Machine _machine;
+    protected Cpu _cpu;
+    protected Memory _memory;
+
+    protected ExecutableFileLoader(Machine machine)
+    {
+        _machine = machine;
+        _cpu = machine.GetCpu();
+        _memory = machine.GetMemory();
+    }
+
+    public abstract byte[] LoadFile(string file, string arguments);
+
+    protected byte[] ReadFile(string file)
+    {
+        return File.ReadAllBytes(file);
+    }
+
+    protected void SetEntryPoint(int cs, int ip)
+    {
+        State state = _cpu.GetState();
+        state.SetCS(cs);
+        state.SetIP(ip);
+        if(_logger.IsEnabled(Serilog.Events.LogEventLevel.Information))
+        {
+            _logger.Information("Program entry point is {@ProgramEntty}", ConvertUtils.ToSegmentedAddressRepresentation(cs, ip));
+        }
+    }
+}
