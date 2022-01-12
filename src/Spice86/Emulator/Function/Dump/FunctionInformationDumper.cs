c0e2fb501d73d83fb2fb4777c3637a2735cc1611
Maximilien Noal <noal.maximilien@gmail.com>
2022-01-12 08:53:37 +0100

    Fixed FuncInfoDumper translation
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/Emulator/Function/Dump/FunctionInformationDumper.cs
+++ src/Spice86/Emulator/Function/Dump/FunctionInformationDumper.cs
@@ -14,41 +14,49 @@ public class FunctionInformationDumper
 {
     public virtual void DumpFunctionHandlers(string destinationFilePath, FunctionInformationToStringConverter converter, StaticAddressesRecorder staticAddressesRecorder, params FunctionHandler[] functionHandlers)
     {
-        IList<FunctionInformation> functionInformations = MergeFunctionHandlers(functionHandlers).ToList();
+        List<FunctionInformation> functionInformations = MergeFunctionHandlers(functionHandlers).ToList();
 
         // Set for search purposes
         HashSet<FunctionInformation> functionInformationsSet = new(functionInformations);
         List<SegmentRegisterBasedAddress> allGlobals = new(staticAddressesRecorder.GetSegmentRegisterBasedAddress());
         HashSet<SegmentedAddress> whiteListOfSegmentForOffset = new(staticAddressesRecorder.GetWhiteListOfSegmentForOffset());
-        var printWriter = new StreamWriter(destinationFilePath);
-        string header = converter.GetFileHeader(allGlobals, whiteListOfSegmentForOffset);
-        if (string.IsNullOrWhiteSpace(header) == false)
+        StreamWriter? printWriter = null;
+        try
         {
-            printWriter.WriteLine(header);
-        }
-
-        foreach (FunctionInformation functionInformation in functionInformations)
-        {
-            if (functionInformation.GetCalledCount() == 0)
+            printWriter = new StreamWriter(destinationFilePath);
+            string header = converter.GetFileHeader(allGlobals, whiteListOfSegmentForOffset);
+            if (string.IsNullOrWhiteSpace(header) == false)
             {
-                continue;
+                printWriter.WriteLine(header);
             }
 
-            string res = converter.Convert(functionInformation, functionInformationsSet);
-            if (string.IsNullOrWhiteSpace(res) == false)
+            foreach (FunctionInformation functionInformation in functionInformations)
             {
-                printWriter.WriteLine(res);
+                if (functionInformation.GetCalledCount() == 0)
+                {
+                    continue;
+                }
+
+                string res = converter.Convert(functionInformation, functionInformationsSet);
+                if (string.IsNullOrWhiteSpace(res) == false)
+                {
+                    printWriter.WriteLine(res);
+                }
+            }
+
+            string footer = converter.GetFileFooter();
+            if (string.IsNullOrWhiteSpace(footer) == false)
+            {
+                printWriter.WriteLine(footer);
             }
         }
-
-        string footer = converter.GetFileFooter();
-        if (string.IsNullOrWhiteSpace(footer) == false)
+        finally
         {
-            printWriter.WriteLine(footer);
+            printWriter?.Dispose();
         }
     }
 
-    private IEnumerable<FunctionInformation> MergeFunctionHandlers(params FunctionHandler[] functionHandlers)
+    private static IEnumerable<FunctionInformation> MergeFunctionHandlers(params FunctionHandler[] functionHandlers)
     {
         return functionHandlers.ToDictionary(x => x.GetFunctionInformations()).Select(x => x.Key.Values).Select(x => x).SelectMany(x => x).Distinct().OrderBy(x => x);
     }
