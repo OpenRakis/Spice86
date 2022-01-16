90ce483c2513e331bc6eb842bce0bd1a9eb4c71c
Maximilien Noal <noal.maximilien@gmail.com>
2022-01-16 16:34:23 +0100

    More translations: GdbCustomCommandHelper.cs
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/Emulator/Gdb/GdbCustomCommandsHandler.cs
+++ src/Spice86/Emulator/Gdb/GdbCustomCommandsHandler.cs
@@ -1,23 +1,21 @@
 ﻿namespace Spice86.Emulator.Gdb;
 
+using Serilog;
+
+using Spice86.Emulator.Cpu;
+using Spice86.Emulator.Devices.Video;
+using Spice86.Emulator.Function;
+using Spice86.Emulator.Function.Dump;
+using Spice86.Emulator.Machine;
+using Spice86.Emulator.Machine.Breakpoint;
+using Spice86.Emulator.Memory;
+using Spice86.Ui;
+using Spice86.Utils;
+
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-
-using Serilog;
-
-using Spice86.Emulator.Devices.Video;
-using Spice86.Emulator.Function.Dump;
-using Spice86.Emulator.Cpu;
-using Spice86.Emulator.Machine;
-using Spice86.Emulator.Memory;
-using Spice86.Emulator.Machine.Breakpoint;
-using Spice86.Utils;
-using Spice86.Emulator.Function;
-using Spice86.Ui;
-
 
 /// <summary>
 /// Handles custom GDB commands triggered in command line via the monitor prefix.<br/>
@@ -26,11 +24,12 @@ using Spice86.Ui;
 /// </summary>
 public class GdbCustomCommandsHandler {
     private static readonly ILogger _logger = Log.Logger.ForContext<GdbCustomCommandsHandler>();
+    private string _defaultDumpDirectory;
     private GdbIo _gdbIo;
     private Machine _machine;
-    private Func<BreakPoint> _onBreakpointReached;
-    private string _defaultDumpDirectory;
-    public GdbCustomCommandsHandler(GdbIo gdbIo, Machine machine, Func<BreakPoint> onBreakpointReached, string defaultDumpDirectory) {
+    private Action<BreakPoint> _onBreakpointReached;
+
+    public GdbCustomCommandsHandler(GdbIo gdbIo, Machine machine, Action<BreakPoint> onBreakpointReached, string defaultDumpDirectory) {
         this._gdbIo = gdbIo;
         this._machine = machine;
         this._onBreakpointReached = onBreakpointReached;
@@ -49,73 +48,23 @@ public class GdbCustomCommandsHandler {
         return ExecuteCustomCommand(customSplit);
     }
 
-    private string ExecuteCustomCommand(string[] customSplit) {
-        throw new NotImplementedException();
-    }
-
-    private string DumpFunctionsCsv(String[] args) {
-        return DumpFunctionWithFormat(args, "Functions.csv", new CsvFunctionInformationToStringConverter());
-    }
-
-    private string DumpFunctions(String[] args) {
-        return DumpFunctionWithFormat(args, "FunctionsDetails.txt", new DetailedFunctionInformationToStringConverter());
-    }
-
-    private string DumpJavaStubs(String[] args) {
-        return DumpFunctionWithFormat(args, "JavaStubs.java", new CSharpStubToStringConverter());
-    }
-
-    private string DumpAll() {
-        string[] args = Array.Empty<string>();
-        DumpMemory(args);
-        DumpFunctionsCsv(args);
-        DumpFunctions(args);
-        DumpJavaStubs(args);
-        return _gdbIo.GenerateMessageToDisplayResponse($"Dumped everything in {_defaultDumpDirectory}");
-    }
-
-    private string PeekRet(String[] args) {
-        if (args.Length == 1) {
-            return _gdbIo.GenerateMessageToDisplayResponse(_machine.PeekReturn());
-        } else {
-            string returnType = args[1];
-            bool parsed = Enum.TryParse(typeof(CallType), returnType, out var callType);
-            if (parsed == false) {
-                return _gdbIo.GenerateMessageToDisplayResponse("Could not understand " + returnType + " as a return type. Valid values are: " + GetValidRetValues());
-            }
-            if(callType is CallType) {
-                return _gdbIo.GenerateMessageToDisplayResponse(_machine.PeekReturn((CallType)callType));
-            }
+    private static double ExtractScale(String[] args) {
+        if (args.Length != 5) {
+            // Not specified in input
+            return 1;
         }
-        return "";
-    }
 
-    private string InvalidCommand(string command) {
-        return Help("Invalid command " + command + "\\n");
-    }
+        string scaleString = args[4];
+        if (!int.TryParse(scaleString, out var value)) {
+            throw new ArgumentException($"Could not parse scale {scaleString}");
+        }
 
-    private string Help(string v) {
-        throw new NotImplementedException();
-    }
+        double scale = Double.Parse(scaleString);
+        if (scale < 0.1) {
+            throw new ArgumentException("Scale cannot be less than 0.1");
+        }
 
-    private string GetValidRetValues() {
-        return string.Join(", ", Enum.GetNames(typeof(CallType)));
-    }
-
-    private string State() {
-        string state = _machine.GetCpu().GetState().ToString();
-        return _gdbIo.GenerateMessageToDisplayResponse(state);
-    }
-
-    private string BreakStop() {
-        BreakPoint breakPoint = new UnconditionalBreakPoint(BreakPointType.MACHINE_STOP, OnBreakpointReached, false);
-        _machine.GetMachineBreakpoints().ToggleBreakPoint(breakPoint, true);
-        _logger.Debug("Breakpoint added for end of execution!\\n{@BreakPoint}", breakPoint);
-        return _gdbIo.GenerateMessageToDisplayResponse("Breakpoint added for end of execution.");
-    }
-
-    private string CallStack() {
-        return _gdbIo.GenerateMessageToDisplayResponse(_machine.DumpCallStack());
+        return scale;
     }
 
     private string BreakCycles(String[] args) {
@@ -131,19 +80,50 @@ public class GdbCustomCommandsHandler {
         long cyclesToWait = long.Parse(cyclesToWaitString);
         long currentCycles = _machine.GetCpu().GetState().GetCycles();
         long cyclesBreak = currentCycles + cyclesToWait;
-        var breakPoint = new BreakPoint(BreakPointType.CYCLES, cyclesBreak, OnBreakpointReached, true);
+        var breakPoint = new BreakPoint(BreakPointType.CYCLES, cyclesBreak, _onBreakpointReached, true);
         _machine.GetMachineBreakpoints().ToggleBreakPoint(breakPoint, true);
         _logger.Debug("Breakpoint added for cycles!\\n{@BreakPoint}", breakPoint);
-        return _gdbIo.GenerateMessageToDisplayResponse("Breakpoint added for cycles. Current cycles is " + currentCycles + ". Will wait for " + cyclesToWait + ". Will stop at " + cyclesBreak);
+        return _gdbIo.GenerateMessageToDisplayResponse($"Breakpoint added for cycles. Current cycles is {currentCycles}. Will wait for {cyclesToWait}. Will stop at {cyclesBreak}");
     }
 
-    private void OnBreakpointReached(BreakPoint obj) {
-        throw new NotImplementedException();
+    private string BreakStop() {
+        BreakPoint breakPoint = new UnconditionalBreakPoint(BreakPointType.MACHINE_STOP, _onBreakpointReached, false);
+        _machine.GetMachineBreakpoints().ToggleBreakPoint(breakPoint, true);
+        _logger.Debug("Breakpoint added for end of execution!\\n{@BreakPoint}", breakPoint);
+        return _gdbIo.GenerateMessageToDisplayResponse("Breakpoint added for end of execution.");
     }
 
-    private string DumpMemory(String[] args) {
-        string fileName = GetFirstArgumentOrDefaultFile(args, "MemoryDump.bin");
-        return DoFileAction(fileName, (f) => _machine.GetMemory().DumpToFile(f), "Error while dumping memory");
+    private string CallStack() {
+        return _gdbIo.GenerateMessageToDisplayResponse(_machine.DumpCallStack());
+    }
+
+    private string DoFileAction(string fileName, Action<string> fileNameConsumer, string errorMessageInCaseIOException) {
+        try {
+            fileNameConsumer.Invoke(fileName);
+        } catch (IOException e) {
+            _logger.Error(e, "{@ErrorMessageInCaseIOException}", errorMessageInCaseIOException);
+            string errorWithException = $"{errorMessageInCaseIOException}: {e.Message}";
+            return _gdbIo.GenerateMessageToDisplayResponse(errorWithException);
+        }
+
+        return ResultIsInFile(fileName);
+    }
+
+    private string DumpAll() {
+        string[] args = Array.Empty<string>();
+        DumpMemory(args);
+        DumpFunctionsCsv(args);
+        DumpFunctions(args);
+        DumpJavaStubs(args);
+        return _gdbIo.GenerateMessageToDisplayResponse($"Dumped everything in {_defaultDumpDirectory}");
+    }
+
+    private string DumpFunctions(String[] args) {
+        return DumpFunctionWithFormat(args, "FunctionsDetails.txt", new DetailedFunctionInformationToStringConverter());
+    }
+
+    private string DumpFunctionsCsv(String[] args) {
+        return DumpFunctionWithFormat(args, "Functions.csv", new CsvFunctionInformationToStringConverter());
     }
 
     private string DumpFunctionWithFormat(String[] args, string defaultSuffix, FunctionInformationToStringConverter converter) {
@@ -154,20 +134,63 @@ public class GdbCustomCommandsHandler {
         }, "Error while dumping functions");
     }
 
-    private string DoFileAction(string fileName, Action<string> fileNameConsumer, string errorMessageInCaseIOException) {
-        try {
-            fileNameConsumer.Invoke(fileName);
-        } catch (IOException e) {
-            _logger.Error(e, "{@ErrorMessageInCaseIOException}", errorMessageInCaseIOException);
-            string errorWithException = errorMessageInCaseIOException + ": " + e.Message;
-            return _gdbIo.GenerateMessageToDisplayResponse(errorWithException);
-        }
-
-        return ResultIsInFile(fileName);
+    private string DumpJavaStubs(String[] args) {
+        return DumpFunctionWithFormat(args, "CSharpStub.cs", new CSharpStubToStringConverter());
     }
 
-    private string ResultIsInFile(string fileName) {
-        return _gdbIo.GenerateMessageToDisplayResponse("Result is in file " + fileName);
+    private string DumpMemory(String[] args) {
+        string fileName = GetFirstArgumentOrDefaultFile(args, "MemoryDump.bin");
+        return DoFileAction(fileName, (f) => _machine.GetMemory().DumpToFile(f), "Error while dumping memory");
+    }
+
+    private string ExecuteCustomCommand(params string[] args) {
+        string originalCommand = args[0];
+        string command = originalCommand.ToLowerInvariant();
+        return command switch {
+            "help" => Help(""),
+            "state" => State(),
+            "breakstop" => BreakStop(),
+            "callstack" => CallStack(),
+            "peekret" => PeekRet(args),
+            "dumpmemory" => DumpMemory(args),
+            "dumpfunctionscsv" => DumpFunctionsCsv(args),
+            "dumpfunctions" => DumpFunctions(args),
+            "dumpjavastubs" => DumpJavaStubs(args),
+            "dumpall" => DumpAll(),
+            "breakcycles" => BreakCycles(args),
+            "vbuffer" => Vbuffer(args),
+            _ => InvalidCommand(originalCommand),
+        };
+    }
+
+    private string ExtractAction(String[] args) {
+        if (args.Length >= 2) {
+            return args[1];
+        }
+
+        throw new ArgumentException("You need to specify an action. Valid actions are [refresh, add, remove]");
+    }
+
+    private int ExtractAddress(String[] args, string action) {
+        if (args.Length < 3) {
+            throw new ArgumentException($"You need to specify an address for action {action}. Format is 0x12AB (hex) or 1234 (decimal)");
+        }
+
+        string addressString = args[2];
+        try {
+            return ParseAddress(addressString);
+        } catch (FormatException nfe) {
+            throw new ArgumentException($"Could not parse address {addressString}", nfe);
+        }
+    }
+
+    private int[] ExtractResolution(String[] args, string action) {
+        if (args.Length < 4) {
+            throw new ArgumentException($"You need to specify a resolution for action {action}. Format is 320x200 for resolution");
+        }
+
+        string resolutionString = args[3];
+        return ParseResolution(resolutionString);
     }
 
     private string GetFirstArgumentOrDefaultFile(String[] args, string defaultSuffix) {
@@ -175,7 +198,84 @@ public class GdbCustomCommandsHandler {
             return args[1];
         }
 
-        return _defaultDumpDirectory + "/spice86dump" + defaultSuffix;
+        return $"{_defaultDumpDirectory}/spice86dump{defaultSuffix}";
+    }
+
+    private string GetValidRetValues() {
+        return string.Join(", ", Enum.GetNames(typeof(CallType)));
+    }
+
+    private string Help(string additinnalMessage) {
+        return _gdbIo.GenerateMessageToDisplayResponse($@"{additinnalMessage}
+            Supported custom commands:
+             -help: display this
+             - dumpall: dumps everything possible in the default directory which is { _defaultDumpDirectory }
+             - dumpMemory < file path to dump >: dump the memory as a binary file
+             - dumpFunctionsCsv < file path to dump >: dump information about the function calls executed in csv format
+             - dumpFunctions < file path to dump >: dump information about the function calls executed with details in human readable format
+             - dumpJavaStubs < file path to dump >: dump java stubs for functions and globals to be used as override
+             - dumpKotlinStubs<file path to dump>: dump kotlin stubs for functions and globals to be used as override
+             - breakCycles<number of cycles to wait before break>: breaks after the given number of cycles is reached
+             - breakStop: setups a breakpoint when machine shuts down
+             - callStack: dumps the callstack to see in which function you are in the VM.
+             - peekRet<optional type>: displays the return address of the current function as stored in the stack in RAM.If a parameter is provided, dump the return on the stack as if the return was one of the provided type. Valid values are: {GetValidRetValues()}
+             - state: displays the state of the machine
+             - vbuffer: family of commands to control video bufers:
+               - vbuffer refresh: refreshes the screen
+               - vbuffer add<address> <resolution> <scale?>: Example vbuffer add 0x1234 320x200 1.5 -> Add an additional buffer displaying what is at address 0x1234, with resolution 320x200 and scale 1.5
+               - vbuffer remove <address>: Deletes the buffer at address
+               - vbuffer list: Lists the buffers currently displayed
+            ");
+    }
+
+    private string InvalidCommand(string command) {
+        return Help($"Invalid command {command}\\n");
+    }
+
+    private int ParseAddress(string address) {
+        if (address.Contains("0x")) {
+            return (int)ConvertUtils.ParseHex32(address);
+        }
+
+        return int.Parse(address);
+    }
+
+    private int[] ParseResolution(string resolution) {
+        string[] split = resolution.Split("x");
+        if (split.Length != 2) {
+            throw new ArgumentException($"Could not parse resolution {resolution}. Format is like 320x200");
+        }
+
+        try {
+            return new int[] { int.Parse(split[0]), int.Parse(split[1]) };
+        } catch (FormatException nfe) {
+            throw new ArgumentException($"Could not parse numbers in resolution {resolution}", nfe);
+        }
+    }
+
+    private string PeekRet(String[] args) {
+        if (args.Length == 1) {
+            return _gdbIo.GenerateMessageToDisplayResponse(_machine.PeekReturn());
+        } else {
+            string returnType = args[1];
+            bool parsed = Enum.TryParse(typeof(CallType), returnType, out var callType);
+            if (parsed == false) {
+                return _gdbIo.GenerateMessageToDisplayResponse($"Could not understand {returnType} as a return type. Valid values are: {GetValidRetValues()}");
+            }
+            if (callType is CallType) {
+                return _gdbIo.GenerateMessageToDisplayResponse(_machine.PeekReturn((CallType)callType));
+            }
+        }
+        return "";
+    }
+
+    private string ResultIsInFile(string fileName) {
+        return _gdbIo.GenerateMessageToDisplayResponse($"Result is in file {fileName}");
+    }
+
+    private string State() {
+        string state = _machine.GetCpu().GetState().ToString();
+        return _gdbIo.GenerateMessageToDisplayResponse(state);
     }
 
     private string Vbuffer(String[] args) {
@@ -199,7 +299,7 @@ public class GdbCustomCommandsHandler {
             int address = ExtractAddress(args, action);
             if ("remove".Equals(action)) {
                 gui.RemoveBuffer(address);
-                return _gdbIo.GenerateMessageToDisplayResponse("Removed buffer at address " + address);
+                return _gdbIo.GenerateMessageToDisplayResponse($"Removed buffer at address {address}");
             }
 
             int[] resolution = ExtractResolution(args, action);
@@ -207,87 +307,16 @@ public class GdbCustomCommandsHandler {
             if ("add".Equals(action)) {
                 VideoBuffer existing = gui.GetVideoBuffers()[address];
                 if (existing != null) {
-                    return _gdbIo.GenerateMessageToDisplayResponse("Buffer already exists: " + existing);
+                    return _gdbIo.GenerateMessageToDisplayResponse($"Buffer already exists: {existing}");
                 }
 
                 gui.AddBuffer(address, scale, resolution[0], resolution[1], null);
-                return _gdbIo.GenerateMessageToDisplayResponse("Added buffer to view address " + address);
+                return _gdbIo.GenerateMessageToDisplayResponse($"Added buffer to view address {address}");
             } else {
-                return _gdbIo.GenerateMessageToDisplayResponse("Could not understand action " + action);
+                return _gdbIo.GenerateMessageToDisplayResponse($"Could not understand action {action}");
             }
         } catch (ArgumentException e) {
             return _gdbIo.GenerateMessageToDisplayResponse(e.Message);
         }
-    }
-
-    private string ExtractAction(String[] args) {
-        if (args.Length >= 2) {
-            return args[1];
-        }
-
-        throw new ArgumentException("You need to specify an action. Valid actions are [refresh, add, remove]");
-    }
-
-    private int ExtractAddress(String[] args, string action) {
-        if (args.Length < 3) {
-            throw new ArgumentException("You need to specify an address for action " + action + ". Format is 0x12AB (hex) or 1234 (decimal)");
-        }
-
-        string addressString = args[2];
-        try {
-            return ParseAddress(addressString);
-        } catch (FormatException nfe) {
-            throw new ArgumentException($"Could not parse address {addressString}", nfe);
-        }
-    }
-
-    private int ParseAddress(string address) {
-        if (address.Contains("0x")) {
-            return (int)ConvertUtils.ParseHex32(address);
-        }
-
-        return int.Parse(address);
-    }
-
-    private int[] ExtractResolution(String[] args, string action) {
-        if (args.Length < 4) {
-            throw new ArgumentException("You need to specify a resolution for action " + action + ". Format is 320x200 for resolution");
-        }
-
-        string resolutionString = args[3];
-        return ParseResolution(resolutionString);
-    }
-
-    private int[] ParseResolution(string resolution) {
-        String[] split = resolution.Split("x");
-        if (split.Length != 2) {
-            throw new ArgumentException("Could not parse resolution " + resolution + ". Format is like 320x200");
-        }
-
-        try {
-            return new int[] { int.Parse(split[0]), int.Parse(split[1]) };
-        } catch (FormatException nfe) {
-            throw new ArgumentException("Could not parse numbers in resolution " + resolution, nfe);
-        }
-    }
-
-    private double ExtractScale(String[] args) {
-        if (args.Length != 5) {
-
-            // Not specified in input
-            return 1;
-        }
-
-        string scaleString = args[4];
-        if (!int.TryParse(scaleString, out var value)) {
-            throw new ArgumentException("Could not parse scale " + scaleString);
-        }
-
-        double scale = Double.Parse(scaleString);
-        if (scale < 0.1) {
-            throw new ArgumentException("Scale cannot be less than 0.1");
-        }
-
-        return scale;
     }
 }
--- src/Spice86/Ui/Gui.cs
+++ src/Spice86/Ui/Gui.cs
@@ -1,14 +1,19 @@
 ﻿namespace Spice86.Ui;
 
 using Avalonia.Input;
+
 using Spice86.Emulator.Devices.Video;
+
 using System;
-using System.Collections.Generic;
 
 /// <summary>
 /// TODO : complete it, along with all the UI framework.
 /// </summary>
 public class Gui {
+
+    internal void AddBuffer(int address, double scale, int v1, int v2, object? p) {
+        throw new NotImplementedException();
+    }
 
     internal void Draw(byte[] vs, Rgb[] rgbs) {
         throw new NotImplementedException();
@@ -30,6 +35,10 @@ public class Gui {
         throw new NotImplementedException();
     }
 
+    internal VideoBuffer[] GetVideoBuffers() {
+        throw new NotImplementedException();
+    }
+
     internal int GetWidth() {
         throw new NotImplementedException();
     }
@@ -43,6 +52,10 @@ public class Gui {
     }
 
     internal bool IsRightButtonClicked() {
+        throw new NotImplementedException();
+    }
+
+    internal void RemoveBuffer(int address) {
         throw new NotImplementedException();
     }
 
@@ -63,18 +76,6 @@ public class Gui {
     }
 
     internal void SetResolution(int videoWidth, int videoHeight, int v) {
-        throw new NotImplementedException();
-    }
-
-    internal void RemoveBuffer(int address) {
-        throw new NotImplementedException();
-    }
-
-    internal VideoBuffer[] GetVideoBuffers() {
-        throw new NotImplementedException();
-    }
-
-    internal void AddBuffer(int address, double scale, int v1, int v2, object p) {
         throw new NotImplementedException();
     }
 }
