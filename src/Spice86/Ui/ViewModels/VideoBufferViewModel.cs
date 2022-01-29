33a72af0e8a938e74532cc76cb44d4250cfbf58c
Maximilien Noal <noal.maximilien@gmail.com>
2022-01-29 09:05:43 +0100

    VideoBufferViewModel is less confusing
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/Ui/ViewModels/VideoBufferViewModel.cs
+++ src/Spice86/Ui/ViewModels/VideoBufferViewModel.cs
@@ -5,30 +5,14 @@ using Avalonia.Controls;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 
-using ReactiveUI;
-
 using Spice86.Emulator.Devices.Video;
 
 using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
-using System.Threading;
 
 public class VideoBufferViewModel : ViewModelBase, IComparable<VideoBufferViewModel>, IDisposable {
-    private uint _address;
-
-    [JsonIgnore]
-    private WriteableBitmap _bitmap = default!;
-
-    public bool IsPrimaryDisplay { get; private set; }
-
     private bool _disposedValue;
-    private int _height;
-    private int _index;
-
-    private double _scalFactor = 1;
-
-    private int _width;
 
     /// <summary>
     /// For AvaloniaUI Designer
@@ -37,58 +21,51 @@ public class VideoBufferViewModel : ViewModelBase, IComparable<VideoBufferViewMo
         if (Design.IsDesignMode == false) {
             throw new InvalidOperationException("This constructor is not for runtime usage");
         }
-        _width = 640;
-        _height = 480;
-        _scalFactor = 1;
+        Width = 640;
+        Height = 480;
+        ScaleFactor = 1;
         var bitmap = new WriteableBitmap(new PixelSize(320, 200), new Vector(96, 96), PixelFormat.Bgra8888, AlphaFormat.Unpremul);
         Bitmap = bitmap;
-        _address = 1;
-        _index = 1;
+        Address = 1;
+        Index = 1;
     }
 
     public VideoBufferViewModel(MainWindowViewModel mainWindowViewModel, int width, int height, double scaleFactor, uint address, int index, bool isPrimaryDisplay) {
         MainWindowViewModel = mainWindowViewModel;
         IsPrimaryDisplay = isPrimaryDisplay;
-        _width = width;
-        _height = height;
-        _scalFactor = scaleFactor;
+        Width = width;
+        Height = height;
+        ScaleFactor = scaleFactor;
         // Todo: compute the dpi parameter from the parent window size?
         var bitmap = new WriteableBitmap(new PixelSize(width, height), new Vector(75, 75), PixelFormat.Bgra8888, AlphaFormat.Unpremul);
         Bitmap = bitmap;
-        _address = address;
-        _index = index;
+        Address = address;
+        Index = index;
     }
 
-    public uint Address => _address;
+    public static event EventHandler? IsDirty;
+
+    public uint Address { get; private set; }
 
     [JsonIgnore]
-    public WriteableBitmap Bitmap {
-        get => _bitmap;
-        set => this.RaiseAndSetIfChanged(ref _bitmap, value);
-    }
+    public WriteableBitmap Bitmap { get; private set; }
 
-    public int Height => _height;
-
+    public int Height { get; private set; }
+    public bool IsPrimaryDisplay { get; private set; }
     public MainWindowViewModel? MainWindowViewModel { get; private set; }
-
-    public double ScaleFactor {
-        get => _scalFactor;
-        set => this.RaiseAndSetIfChanged(ref _scalFactor, value);
-    }
-
-    public int Width => _width;
+    public double ScaleFactor { get; private set; }
+    public int Width { get; private set; }
+    private int Index { get; set; }
 
     public int CompareTo(VideoBufferViewModel? other) {
-        if (_index < other?._index) {
+        if (Index < other?.Index) {
             return -1;
-        } else if (_index == other?._index) {
+        } else if (Index == other?.Index) {
             return 0;
         } else {
             return 1;
         }
     }
-
-    public static event EventHandler? IsDirty;
 
     public void Dispose() {
         // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
@@ -97,19 +74,18 @@ public class VideoBufferViewModel : ViewModelBase, IComparable<VideoBufferViewMo
     }
 
     public unsafe void Draw(byte[] memory, Rgb[] palette) {
-        int size = _width * _height;
-        long endAddress = _address + size;
-        uint startAddress = _address;
+        int size = Width * Height;
+        long endAddress = Address + size;
         var buffer = new List<uint>(size);
-        for (long i = startAddress; i < endAddress; i++) {
+        for (long i = Address; i < endAddress; i++) {
             byte colorIndex = memory[i];
             Rgb pixel = palette[colorIndex];
             uint argb = pixel.ToArgb();
             buffer.Add(argb);
         }
-        if(_disposedValue == false) {
-            using ILockedFramebuffer? buf = Bitmap.Lock();
-            uint* dst = (uint*)buf?.Address;
+        if (_disposedValue == false) {
+            using ILockedFramebuffer buf = Bitmap.Lock();
+            uint* dst = (uint*)buf.Address;
             for (int i = 0; i < size; i++) {
                 uint argb = buffer[i];
                 dst[i] = argb;
@@ -119,15 +95,15 @@ public class VideoBufferViewModel : ViewModelBase, IComparable<VideoBufferViewMo
     }
 
     public override bool Equals(object? obj) {
-        return this == obj || (obj is VideoBufferViewModel other) && _index == other._index;
+        return this == obj || (obj is VideoBufferViewModel other) && Index == other.Index;
     }
 
     public override int GetHashCode() {
-        return _index;
+        return Index;
     }
 
     public int GetIndex() {
-        return _index;
+        return Index;
     }
 
     public override string? ToString() {
@@ -137,7 +113,7 @@ public class VideoBufferViewModel : ViewModelBase, IComparable<VideoBufferViewMo
     protected virtual void Dispose(bool disposing) {
         if (!_disposedValue) {
             if (disposing) {
-                _bitmap.Dispose();
+                Bitmap.Dispose();
             }
             _disposedValue = true;
         }
