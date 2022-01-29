9fb15a3567c4de8447c0ec692b47f77e3fad2035
Maximilien Noal <noal.maximilien@gmail.com>
2022-01-29 08:39:03 +0100

    Remove 3rd Draw Thread
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/Ui/ViewModels/MainWindowViewModel.cs
+++ src/Spice86/Ui/ViewModels/MainWindowViewModel.cs
@@ -28,9 +28,7 @@ using System.Threading;
 public class MainWindowViewModel : ViewModelBase, IVideoKeyboardMouseIO, IDisposable {
     private static readonly ILogger _logger = Log.Logger.ForContext<MainWindowViewModel>();
     private readonly Configuration? _configuration;
-    private readonly AutoResetEvent nextFrame = new AutoResetEvent(false);
     private bool _disposedValue;
-    private Thread? _drawThread;
     private Thread? _emulatorThread;
     private long _frameNumber = 0;
     private int _height = 1;
@@ -48,8 +46,6 @@ public class MainWindowViewModel : ViewModelBase, IVideoKeyboardMouseIO, IDispos
     private AvaloniaList<VideoBufferViewModel> _videoBuffers = new();
     private int _width = 1;
 
-    private FrameEventArgs? frame;
-
     public MainWindowViewModel() {
         if (Design.IsDesignMode) {
             return;
@@ -61,10 +57,7 @@ public class MainWindowViewModel : ViewModelBase, IVideoKeyboardMouseIO, IDispos
         }
         MainTitle = $"{nameof(Spice86)} {configuration?.Exe}";
         SetResolution(320, 200, 0);
-        this.NextFrame += OnNextFrame;
     }
-
-    private event NextFrameEventHandler? NextFrame;
 
     public string? MainTitle { get; private set; }
 
@@ -85,7 +78,15 @@ public class MainWindowViewModel : ViewModelBase, IVideoKeyboardMouseIO, IDispos
     }
 
     public void Draw(byte[] memory, Rgb[] palette) {
-        this.NextFrame?.Invoke(new FrameEventArgs(memory, palette, _frameNumber, SortedBuffers()));
+        _frameNumber++;
+        if (_disposedValue || _isSettingResolution) {
+            return;
+        }
+        foreach (VideoBufferViewModel videoBuffer in SortedBuffers()) {
+            {
+                videoBuffer.Draw(memory, palette);
+            }
+        }
     }
 
     public void Exit() {
@@ -151,8 +152,6 @@ public class MainWindowViewModel : ViewModelBase, IVideoKeyboardMouseIO, IDispos
 
     public void OnMainWindowOpened(object? sender, EventArgs e) {
         if (sender is Window) {
-            _drawThread = new Thread(Draw);
-            _drawThread.Start();
             _emulatorThread = new Thread(RunMachine);
             _emulatorThread.Start();
         }
@@ -213,26 +212,10 @@ public class MainWindowViewModel : ViewModelBase, IVideoKeyboardMouseIO, IDispos
     protected virtual void Dispose(bool disposing) {
         if (!_disposedValue) {
             if (disposing) {
-                foreach (VideoBufferViewModel buffer in VideoBuffers) {
-                    buffer.Dispose();
-                }
+                DisposeBuffers();
                 _programExecutor?.Dispose();
             }
             _disposedValue = true;
-        }
-    }
-
-    private void Draw() {
-        while (true) {
-            nextFrame.WaitOne(1000);
-            if (frame is null || _disposedValue || _isSettingResolution) {
-                continue;
-            }
-            foreach (VideoBufferViewModel videoBuffer in frame.SortedBuffers) {
-                {
-                    videoBuffer.Draw(frame.Memory, frame.Palette);
-                }
-            }
         }
     }
 
@@ -246,8 +229,6 @@ public class MainWindowViewModel : ViewModelBase, IVideoKeyboardMouseIO, IDispos
         if (_isSettingResolution || _disposedValue) {
             return;
         }
-        Interlocked.Exchange(ref this.frame, e);
-        this.nextFrame.Set();
     }
 
     private void RunOnKeyEvent(Action? runnable) {
