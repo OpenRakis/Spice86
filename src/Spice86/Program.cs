22faef10f4bf0b54610ad74b806f5e17af9639f4
Maximilien Noal <noal.maximilien@gmail.com>
2022-01-29 20:36:05 +0100

    Re-enable Info log level
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/Program.cs
+++ src/Spice86/Program.cs
@@ -17,7 +17,7 @@ internal class Program {
 
     private static readonly ILogger _logger = new LoggerConfiguration()
         .WriteTo.Console()
-        .MinimumLevel.Debug()
+        .MinimumLevel.Information()
         .CreateLogger();
 
     static Program() {
