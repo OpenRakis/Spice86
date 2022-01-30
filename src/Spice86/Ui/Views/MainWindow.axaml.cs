1cd2ffd3c8373fb71d4d334ed6f1bb67263abccc
Maximilien Noal <noal.maximilien@gmail.com>
2022-01-30 09:18:34 +0100

    Avoid error on exit, with event
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/Ui/Views/MainWindow.axaml.cs
+++ src/Spice86/Ui/Views/MainWindow.axaml.cs
@@ -4,13 +4,23 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
+using System;
+using System.ComponentModel;
+
 public partial class MainWindow : Window {
 
     public MainWindow() {
         InitializeComponent();
+        this.Closing += MainWindow_Closing;
 #if DEBUG
         this.AttachDevTools();
 #endif
+    }
+
+    public static event EventHandler<CancelEventArgs>? AppClosing;
+
+    private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e) {
+        AppClosing?.Invoke(sender, e);
     }
 
     private void InitializeComponent() {
