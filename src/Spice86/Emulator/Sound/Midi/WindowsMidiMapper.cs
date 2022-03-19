8b62715ff6e2660e7c89af2d0bc92ffd717b08f2
Maximilien Noal <noal.maximilien@gmail.com>
2022-03-19 23:01:39 +0100

    Windows MIDI: don't pause, the music becomes forever silent
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/Emulator/Sound/Midi/WindowsMidiMapper.cs
+++ src/Spice86/Emulator/Sound/Midi/WindowsMidiMapper.cs
@@ -15,8 +15,15 @@ internal sealed class WindowsMidiMapper : MidiDevice {
 
     protected override void PlayShortMessage(uint message) => NativeMethods.midiOutShortMsg(midiOutHandle, message);
     protected override void PlaySysex(ReadOnlySpan<byte> data) { }
-    public override void Pause() => NativeMethods.midiOutReset(midiOutHandle);
-    public override void Resume() { }
+    public override void Pause() {
+        // ... don't pause ...
+        //NativeMethods.midiOutReset(midiOutHandle);
+    }
+
+    public override void Resume() {
+        // ... and restart, the music becomes forever silent !
+        //NativeMethods.midiOutOpen(out midiOutHandle, NativeMethods.MIDI_MAPPER, IntPtr.Zero, IntPtr.Zero, 0);
+    }
 
     protected override void Dispose(bool disposing) {
         if (midiOutHandle != IntPtr.Zero) {
