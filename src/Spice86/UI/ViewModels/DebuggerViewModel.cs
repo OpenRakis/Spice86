5fff64779fa73344ec783ebdd11b1e891c1406bd
Maximilien Noal <noal.maximilien@gmail.com>
2022-05-12 08:56:53 +0200

    Debugger with DataGrids
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/UI/ViewModels/DebuggerViewModel.cs
+++ src/Spice86/UI/ViewModels/DebuggerViewModel.cs
@@ -2,20 +2,21 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.Threading;
 
 using CommunityToolkit.Mvvm.ComponentModel;
 
+using Spice86.Emulator.CPU;
 using Spice86.Emulator.VM;
 
 public partial class DebuggerViewModel : ObservableObject {
-    [ObservableProperty]
-    private Machine? _machine;
     private DispatcherTimer? _timer;
 
     public DebuggerViewModel() {
@@ -24,17 +25,19 @@ public partial class DebuggerViewModel : ObservableObject {
         }
     }
 
-
+    [ObservableProperty]
+    private AvaloniaList<Machine>? _machine = new();
 
     public DebuggerViewModel(Machine machine) {
-        _machine = machine;
+        _machine = new(machine);
         _timer = new(TimeSpan.FromMilliseconds(200), DispatcherPriority.Normal, UpdateMachine);
         _timer.Start();
     }
 
     public void UpdateMachine(object? sender, EventArgs e) {
-        Machine? machine = _machine;
-        Machine = null;
-        Machine = machine;
+        if (_machine is null) {
+            return;
+        }
+        Machine = new(_machine);
     }
 }
