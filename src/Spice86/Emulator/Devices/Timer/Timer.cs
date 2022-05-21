14cd9cc64568d92951f2f9064c5a3554e9fefe2c
Maximilien Noal <noal.maximilien@gmail.com>
2022-05-21 12:15:42 +0200

    Pic uses its IRQs to Vectors table to process IRQs
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/Emulator/Devices/Timer/Timer.cs
+++ src/Spice86/Emulator/Devices/Timer/Timer.cs
@@ -109,7 +109,7 @@ public class Timer : DefaultIOPortHandler {
     public void Tick() {
         long cycles = _cpu.State.Cycles;
         if (_counters[0].ProcessActivation(cycles)) {
-            _pic.ProcessInterrupt(0x8);
+            _pic.ProcessInterruptVector(0x8);
         }
 
         if (_vgaCounter.ProcessActivation(cycles)) {
