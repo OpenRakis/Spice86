f30d25d62f45ed041eb0baef66cef61d2370e2e0
Maximilien Noal <noal.maximilien@gmail.com>
2022-07-24 10:58:11 +0200

    OpenAL: fixed 30 seconds delay (reverted backend)
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/Backend/Audio/OpenAl/OpenAlAudioPlayer.cs
+++ src/Spice86/Backend/Audio/OpenAl/OpenAlAudioPlayer.cs
@@ -1,25 +1,23 @@
 namespace Spice86.Backend.Audio.OpenAl;
 
-using Serilog;
-
 using Silk.NET.OpenAL;
 
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Versioning;
-
 [UnsupportedOSPlatform("browser")]
 public sealed unsafe class OpenAlAudioPlayer : AudioPlayer {
-    private readonly ILogger _logger = Program.Logger.ForContext<OpenAlAudioPlayer>();
     private const int MaxAlBuffers = 100;
-    private static AL? _al;
-    private static ALContext? _alContext;
+    private const int OpenAlBufferModulo = 16;
+    private readonly Stack<byte[]> _backBuffer = new();
+    private static AL? _al = null;
+    private static ALContext? _alContext = null;
     private static Device* _device = null;
     private static Context* _context = null;
     private readonly uint _source = 0;
     private bool _disposed = false;
-    private const BufferFormat OpenAlBufferFormat = BufferFormat.Stereo16;
+    private const BufferFormat _openAlBufferFormat = BufferFormat.Stereo16;
     private readonly List<uint> _alBuffers = new();
     private static int _numberOfOpenAlPlayerInstances = 0;
     private bool _started;
@@ -30,9 +28,13 @@ public sealed unsafe class OpenAlAudioPlayer : AudioPlayer {
             _al.GetError();
             _alContext ??= ALContext.GetApi(true);
         } catch {
-            _al ??= AL.GetApi(false);
-            _al.GetError();
-            _alContext ??= ALContext.GetApi(false);
+            try {
+                _al ??= AL.GetApi(false);
+                _al.GetError();
+                _alContext ??= ALContext.GetApi(false);
+            } catch {
+                return;
+            }
         }
 
         if (_device is null) {
@@ -53,6 +55,7 @@ public sealed unsafe class OpenAlAudioPlayer : AudioPlayer {
             if (_context != null) {
                 _alContext.DestroyContext(_context);
             }
+
             _alContext.CloseDevice(_device);
             _al?.Dispose();
             _alContext.Dispose();
@@ -72,9 +75,10 @@ public sealed unsafe class OpenAlAudioPlayer : AudioPlayer {
         _numberOfOpenAlPlayerInstances++;
     }
 
-    private uint GenerateNewOpenAlBuffer() {
+    private uint GenerateNewBuffer()
+    {
         if (_al is null) {
-            throw new NullReferenceException(nameof(_al));
+            throw new NotSupportedException("GenerateNewBuffer was called without a valid OpenAL instance.");
         }
         uint buffer = _al.GenBuffer();
         ThrowIfAlError();
@@ -82,24 +86,17 @@ public sealed unsafe class OpenAlAudioPlayer : AudioPlayer {
         return buffer;
     }
 
-    public void Reset() {
-        if (_source == 0) {
-            throw new NotSupportedException($"{nameof(Reset)} was called without a valid source.");
-        }
-        _al?.SetSourceProperty(_source, SourceInteger.Buffer, 0u);
-    }
-
     protected override void Start(bool useCallback) {
         if (_source == 0) {
-            throw new NotSupportedException($"{nameof(Start)} was called without a valid source.");
+            throw new NotSupportedException("Start was called without a valid source.");
         }
-        _started = true;
         Play();
+        _started = true;
     }
 
     private void Play() {
         SourceState currentState = GetSourceState();
-        if (currentState == SourceState.Playing && _started) {
+        if (currentState == SourceState.Playing) {
             return;
         }
         _al?.SourcePlay(_source);
@@ -109,16 +106,12 @@ public sealed unsafe class OpenAlAudioPlayer : AudioPlayer {
     private SourceState GetSourceState() {
         int state = 0;
         _al?.GetSourceProperty(_source, GetSourceInteger.SourceState, out state);
-        var currentState = (SourceState) state;
-        if (currentState == SourceState.Stopped) {
-            //_logger.Warning("{@Source} was not playing, it is {@State}", _source, currentState);
-        }
-        return currentState;
+        return (SourceState) state;
     }
 
     protected override void Stop() {
         if (_source == 0) {
-            throw new NotSupportedException($"{nameof(Stop)} was called without a valid source.");
+            throw new NotSupportedException("Stop was called without a valid source.");
         }
         _al?.SourceStop(_source);
         _started = false;
@@ -132,32 +125,103 @@ public sealed unsafe class OpenAlAudioPlayer : AudioPlayer {
             throw new NullReferenceException(nameof(_al));
         }
         _al.GetSourceProperty(_source, GetSourceInteger.BuffersProcessed, out int processed);
+        int remainingLength = GetRemainingLength(input.Length);
         if (processed > 0) {
-            uint buffer = 0;
-            _al.SourceUnqueueBuffers(_source, 1, &buffer);
-            _al.GetError();
-            BufferData(buffer, input);
+            byte[]? data = null;
+            byte[] inputToArray = input.ToArray();
+            while (processed > 0 && remainingLength > 0) {
+                uint buffer = 0;
+                _al.SourceUnqueueBuffers(_source, 1, &buffer);
+                _al.GetError();
+                if (!TryEnqueueData(buffer, inputToArray, ref data, ref remainingLength)) {
+                    break;
+                }
+                processed--;
+            }
         } else if(_alBuffers.Count < MaxAlBuffers) {
-            uint buffer = GenerateNewOpenAlBuffer();
-            BufferData(buffer, input);
+            byte[]? data = null;
+            byte[] inputToArray = input.ToArray();
+            while (remainingLength > 0) {
+                uint buffer = GenerateNewBuffer();
+                if (!TryEnqueueData(buffer, inputToArray, ref data, ref remainingLength)) {
+                    break;
+                }
+            }
         } else {
             return 0;
         }
-        return input.Length;
-    }
-
-    private void BufferData(uint buffer, ReadOnlySpan<byte> input) {
-        if (buffer == 0) {
-            throw new NotSupportedException($"${nameof(BufferData)} was called without a valid ${nameof(buffer)}.");
+        if(_started) {
+            Play();
         }
-        _al?.BufferData(buffer, OpenAlBufferFormat, input.ToArray(), Format.SampleRate);
-        ThrowIfAlError();
-        _al?.SourceQueueBuffers(_source, 1, &buffer);
-        ThrowIfAlError();
-        Play();
+        return remainingLength > input.Length ? input.Length : remainingLength;
+    }
+    private static int GetRemainingLength(int length)
+    {
+        if (length == 0) {
+            return 0;
+        }
+        int remainingLength = length - (length % OpenAlBufferModulo);
+        if (remainingLength == 0) {
+            remainingLength = length;
+        }
+        return remainingLength;
     }
 
-    private void ThrowIfAlError() {
+    private bool TryEnqueueData(uint buffer, byte[] inputToArray, ref byte[]? data, ref int remainingLength)
+    {
+        if (buffer == 0 || inputToArray.Length - data?.Length <= 0)
+        {
+            return false;
+        }
+
+        if (data is null)
+        {
+            data = inputToArray;
+        }
+        else
+        {
+            data = inputToArray[..data.Length];
+        }
+        if(_backBuffer.TryPeek(out _))
+        {
+            while (_backBuffer.TryPop(out byte[]? bytes)) {
+                byte[] newData = new byte[bytes.Length + data.Length];
+                Array.Copy(bytes, newData, bytes.Length);
+                Array.Copy(data, 0, newData, bytes.Length, data.Length);
+                data = newData;
+            }
+            return TryQueueBuffer(buffer, data);
+        }
+        else
+        {
+            remainingLength = GetRemainingLength(data.Length);
+            byte[] currentBytes = data[0..remainingLength];
+            return TryQueueBuffer(buffer, currentBytes);
+        }
+    }
+
+    private bool TryQueueBuffer(uint buffer, byte[] currentBytes)
+    {
+        if (TryBufferData(buffer, currentBytes)) {
+            _al?.SourceQueueBuffers(_source, 1, &buffer);
+            ThrowIfAlError();
+            return true;
+        }
+        return false;
+    }
+
+    private bool TryBufferData(uint buffer, byte[] data)
+    {
+        _al?.BufferData(buffer, _openAlBufferFormat, data, Format.SampleRate);
+        if (_al?.GetError() == AudioError.InvalidValue)
+        {
+            _backBuffer.Push(data);
+            return false;
+        }
+        return true;
+    }
+
+    private static void ThrowIfAlError() {
         AudioError? error = _al?.GetError();
         if(error is AudioError.InvalidValue) {
             throw new InvalidDataException(error.ToString());
