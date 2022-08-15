22a9f15498483199ee12f3c8cb2856b4894e3b67
Maximilien Noal <noal.maximilien@gmail.com>
2022-08-15 10:16:46 +0200

    xplat audio: PortAudio replaces OpenAL. Finally something that works.
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Backend/Audio/PortAudio/PortAudioPlayer.cs
+++ src/Spice86.Core/Backend/Audio/PortAudio/PortAudioPlayer.cs
@@ -4,28 +4,25 @@ using Bufdio.Engines;
 namespace Spice86.Core.Backend.Audio.PortAudio; 
 
 public class PortAudioPlayer : AudioPlayer {
-    private static IAudioEngine? _engine;
+    private readonly IAudioEngine _engine;
     private bool _disposed;
     private static int _numberOfPortAudioPlayerInstances = 0;
     private PortAudioPlayer(AudioFormat format) : base(format) {
         BufdioLib.InitializePortAudio();
         AudioEngineOptions options = new AudioEngineOptions(2, format.SampleRate);
-        _engine ??= new PortAudioEngine(options);
+        _engine = new PortAudioEngine(options);
         _numberOfPortAudioPlayerInstances++;
     }
 
     public static PortAudioPlayer Create() {
         return new PortAudioPlayer(new AudioFormat(SampleRate: 48000, Channels: 2,
-            SampleFormat: SampleFormat.SignedPcm16));
+            SampleFormat: SampleFormat.IeeeFloat32));
     }
 
     protected override void Dispose(bool disposing) {
         if (!_disposed) {
             if (disposing) {
-                if (_numberOfPortAudioPlayerInstances == 1) {
-                    _engine?.Dispose();
-                }
-                _numberOfPortAudioPlayerInstances--;
+                _engine.Dispose();
                 _disposed = true;
             }
         }
@@ -40,9 +37,8 @@ public class PortAudioPlayer : AudioPlayer {
     }
 
     protected override int WriteDataInternal(ReadOnlySpan<byte> data) {
-        Span<byte> destination = new Span<byte>(data.ToArray());
-        Span<float> samples = SpanExtensions.Cast<byte, float>(destination);
-        _engine?.Send(samples);
+        ReadOnlySpan<float> samples = data.Cast<byte, float>();
+        _engine.Send(new(samples.ToArray()));
         return data.Length;
     }
 }
