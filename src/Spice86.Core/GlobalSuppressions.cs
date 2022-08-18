2587131692cd9a110a6a147bc7312d0e320bc3a9
Maximilien Noal <noal.maximilien@gmail.com>
2022-08-18 22:16:23 +0200

    GlobalSuppressions: removed two obsolete lines
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/GlobalSuppressions.cs
+++ src/Spice86.Core/GlobalSuppressions.cs
@@ -7,5 +7,3 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "This barely affects performance, and breaks the public APIs contract if the method is public")]
 [assembly: SuppressMessage("Style", "IDE0007:Use implicit type", Justification = "We don't like 'var' around these parts, partner...")]
-[assembly: SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "Nah", Scope = "member", Target = "~M:Spice86.Emulator.ProgramExecutor.InitializeDos(Spice86.Emulator.Configuration)")]
-[assembly: SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "Nah", Scope = "member", Target = "~M:Spice86.Emulator.ProgramExecutor.LoadFileToRun(Spice86.Emulator.Configuration)")]
