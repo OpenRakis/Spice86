77b6f0cff8a9b27f2a451b53279a39ee6f638781
Maximilien Noal <noal.maximilien@gmail.com>
2022-08-18 21:56:41 +0200

    Serilog: output exceptions into JSON
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Logging/Serilogger.cs
+++ src/Spice86.Logging/Serilogger.cs
@@ -3,9 +3,9 @@
 using Serilog;
 using Serilog.Core;
 using Serilog.Events;
+using Serilog.Exceptions;
 
-public class Serilogger
-{
+public class Serilogger {
     private const string LogFormat = "[{Timestamp:HH:mm:ss} {Level:u3} {Properties}] {Message:lj}{NewLine}{Exception}";
     public static LoggingLevelSwitch LogLevelSwitch { get; set; } = new(LogEventLevel.Warning);
 
@@ -13,11 +13,12 @@ public class Serilogger
 
     static Serilogger() {
         _loggerInstance = new LoggerConfiguration()
+        .Enrich.WithExceptionDetails()
         .WriteTo.Console(outputTemplate: LogFormat)
         .WriteTo.Debug(outputTemplate: LogFormat)
         .MinimumLevel.ControlledBy(LogLevelSwitch)
         .CreateLogger();
-}
+    }
 
     public ILogger Logger => _loggerInstance;
 
