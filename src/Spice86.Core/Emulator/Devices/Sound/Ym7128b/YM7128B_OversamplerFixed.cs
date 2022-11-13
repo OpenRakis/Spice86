9e719ac11a379d5ae5cda165659ae031c39b5eff
Maximilien Noal <noal.maximilien@gmail.com>
2022-11-13 23:24:38 +0100

    More YM7128B translations
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/Devices/Sound/Ym7128b/YM7128B_OversamplerFixed.cs
+++ src/Spice86.Core/Emulator/Devices/Sound/Ym7128b/YM7128B_OversamplerFixed.cs
@@ -1,7 +1,7 @@
 ﻿namespace Spice86.Core.Emulator.Devices.Sound.Ym7128b;
 public struct YM7128B_OversamplerFixed {
     public YM7128B_OversamplerFixed() {
-        buffer_ = new short[(int)YM7128B_OversamplerSpecs.YM7128B_Oversampler_Length];
+        Buffer = new short[(int)YM7128B_OversamplerSpecs.YM7128B_Oversampler_Length];
     }
-    public short[] buffer_;
+    public short[] Buffer {get; set;}
 }
---
+++ src/Spice86.Core/Emulator/Devices/Sound/Ym7128b/YM7128B_OversamplerFloat.cs
@@ -0,0 +1,7 @@
+namespace Spice86.Core.Emulator.Devices.Sound.Ym7128b;
+public struct YM7128B_OversamplerFloat {
+    public YM7128B_OversamplerFloat() {
+        Buffer = new double[(int)YM7128B_OversamplerSpecs.YM7128B_Oversampler_Length];
+    }
+    public double[] Buffer {get; set;}
+}
