d84cb2c356bad49fdbf6ff0127b70d1702639a10
Maximilien Noal <noal.maximilien@gmail.com>
2023-01-10 19:26:08 +0100

    Experimental XMS support
    XMS and EMS mamagers from Aeon
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    MetaAllocator: optimized LINQ statement
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Formatted new code. Made all classes and structs public
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Memory: Made XMS and EMS Managers public APIs
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Configurable XMS and EMS presence
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    XMS and EMS don't mix
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Machine: Register the XMS and EMS services
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    EMS is also a DeviceCallbackProvider
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Revert "EMS is also a DeviceCallbackProvider"
    
    This reverts commit 24fac164f7115853635bab2ae79e9ba3fd2632e1.
    
    Removed done TODO
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    XMS: Use Spice86 exceptions
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Formatting
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Removed warning about memory paging. Don't care.
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    EmsFunctions is now an enum
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Revert "EmsFunctions is now an enum"
    
    This reverts commit 67afed499b5c417a1e22eb32b08092ddf4f41497.
    
    Moved Ems and Xms into DOS interrupt handlers namespace
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Xms, Ems: Added some TODOs.
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    More XMS and EMS fixups
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    XMS: Dispatch Table implemented
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Moved Xms into its own branch and PR
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    XmsVirtualDevice installed
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Install XMS Callback Int à 0xFF
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Xms: fixed off by one error
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Xms: Initialize memory map
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Bios: CopyXMS method
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    Fixed build errors
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    XMS: Removed XmsVirtualDevice
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    ReservedBlock: Removed unused class
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    XMS: Fixed post-rebase build failure
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    XMS: Added character device initialization
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    XMS: Removed two passes initialization code
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    XMS: Resloved some warnings
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    XMS: Enabled A20 Gate related code
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    INT15H: Report XMS size as 8MB
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    INT2F: Provide XMS driver interrupt method
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    INT15: Protect the HMA if A20 Gate is enabled
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    XMS: Control Function installed
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    XMS: Set XMS Memory Size at 8 MB
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    XMS: Map XMS Memory
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    INT2F: depends on XMS driver
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ src/Spice86.Core/Emulator/InterruptHandlers/Dos/Xms/XmsMoveData.cs
@@ -0,0 +1,51 @@
+﻿namespace Spice86.Core.Emulator.InterruptHandlers.Dos.Xms;
+
+using Spice86.Shared.Emulator.Memory;
+
+using System.Runtime.InteropServices;
+
+using Spice86.Core.Emulator.Memory;
+using Spice86.Shared.Emulator.Memory;
+using Spice86.Shared;
+using Spice86.Shared.Emulator.Memory;
+
+/// <summary>
+/// In-memory structure with information about an XMS move request.
+/// </summary>
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct XmsMoveData {
+    /// <summary>
+    /// Number of bytes to move.
+    /// </summary>
+    public uint Length;
+
+    /// <summary>
+    /// Handle of source block; zero if moving from segment:offset pair.
+    /// </summary>
+    public ushort SourceHandle;
+
+    /// <summary>
+    /// Source offset as a 32-bit value or a segment:offset pair.
+    /// </summary>
+    public uint SourceOffset;
+
+    /// <summary>
+    /// Handle of destination block; zero if moving to segment:offset pair.
+    /// </summary>
+    public ushort DestHandle;
+
+    /// <summary>
+    /// Destination offset as a 32-bit value or a segment:offset pair.
+    /// </summary>
+    public uint DestOffset;
+
+    /// <summary>
+    /// Gets the source address as a segment:offset value.
+    /// </summary>
+    public SegmentedAddress SourceAddress => new((ushort)(SourceOffset >> 16), (ushort)SourceOffset);
+
+    /// <summary>
+    /// Gets the destination address as a segment:offset value.
+    /// </summary>
+    public SegmentedAddress DestAddress => new((ushort)(DestOffset >> 16), (ushort)DestOffset);
+}
