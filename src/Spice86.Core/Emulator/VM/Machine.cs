e3c89cc8ec2bd08620d601d52327722a6d4dacfb
Maximilien Noal <noal.maximilien@gmail.com>
2023-01-11 22:33:20 +0100

    Revert "EMS is also a DeviceCallbackProvider"
    This reverts commit 24fac164f7115853635bab2ae79e9ba3fd2632e1.

--- src/Spice86.Core/Emulator/VM/Machine.cs
+++ src/Spice86.Core/Emulator/VM/Machine.cs
@@ -211,8 +211,7 @@ public class Machine : IDisposable {
             Register((ICallback)Xms);
         }
         if(Ems is not null) {
-            Register((IDeviceCallbackProvider)Ems);
-            Register((ICallback)Ems);
+            Register(Ems);
         }
     }
 
