f91b6275ed6313667a0a94fc35d028baff6a2213
Maximilien Noal <noal.maximilien@gmail.com>
2023-01-11 18:11:13 +0100

    XMS and EMS don't mix
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/VM/Machine.cs
+++ src/Spice86.Core/Emulator/VM/Machine.cs
@@ -127,6 +127,9 @@ public class Machine : IDisposable {
 
         Memory = new Memory(this, sizeInKb: (uint)Configuration.Kilobytes);
         Cpu = new Cpu(this, new ServiceProvider().GetLoggerForContext<Cpu>(), executionFlowRecorder, recordData);
+        if(configuration.Xms && configuration.Ems) {
+            throw new UnrecoverableException("Cannot have XMS and EMS at the same time");
+        }
         if(configuration.Xms) {
             Xms = new(this);
         }
