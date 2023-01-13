c370a0db9ae129998305c8984a7e226f9a9d4b06
Maximilien Noal <noal.maximilien@gmail.com>
2023-01-13 12:28:45 +0100

    VideoModes: fixed and extended EGA graphics modes entries
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/Devices/Video/VideoModes.cs
+++ src/Spice86.Core/Emulator/Devices/Video/VideoModes.cs
@@ -2,12 +2,17 @@ namespace Spice86.Core.Emulator.Devices.Video;
 
 public enum VideoModes {
     /// <summary>
-    /// 640x400, 16 colors fixed palette, 0xA000 offset in main memory. Planar.
+    /// 320x200, 16 colors from a dynamic palette, mapped at 0xA000 offset in main memory, for a length of 64 KB. Planar.
     /// </summary>
-    Ega2bpp640x400 = 0x0D,
+    Ega4bpp320x200 = 0x0D,
 
     /// <summary>
-    /// 320x200, 256 colors dynamic palette, 0xA000 offset in main memory. Planar.
+    /// 640x400, 16 colors from a dynamic palette, mapped at 0xA000 offset in main memory, for a length of 64 KB. Planar.
+    /// </summary>
+    Ega4bpp640x400 = 0x0E,
+
+    /// <summary>
+    /// 320x200, 256 colors from a dynamic palette, mapped at 0xA000 offset in main memory. Planar.
     /// </summary>
     Vga8bpp320x200 = 0x13,
 }
