057e387b8ec141202304b853aabc257931435165
Maximilien Noal <noal.maximilien@gmail.com>
2023-01-13 12:32:19 +0100

    There is no 640x400 EGA mode
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/Devices/Video/VideoModes.cs
+++ src/Spice86.Core/Emulator/Devices/Video/VideoModes.cs
@@ -2,14 +2,14 @@ namespace Spice86.Core.Emulator.Devices.Video;
 
 public enum VideoModes {
     /// <summary>
-    /// 320x200, 16 colors from a dynamic palette, mapped at 0xA000 offset in main memory, for a length of 64 KB. Planar.
+    /// 320x200, 16 colors from a fixed palette, mapped at 0xA000 offset in main memory, for a length of 64 KB. Planar.
     /// </summary>
     Ega4bpp320x200 = 0x0D,
 
     /// <summary>
-    /// 640x400, 16 colors from a dynamic palette, mapped at 0xA000 offset in main memory, for a length of 64 KB. Planar.
+    /// 640x200, 16 colors from a fixed palette, mapped at 0xA000 offset in main memory, for a length of 64 KB. Planar.
     /// </summary>
-    Ega4bpp640x400 = 0x0E,
+    Ega4bpp640x200 = 0x0E,
 
     /// <summary>
     /// 320x200, 256 colors from a dynamic palette, mapped at 0xA000 offset in main memory. Planar.
