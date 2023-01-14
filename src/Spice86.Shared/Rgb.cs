1524d35b5a53ef36c41645107f62aa98e1762f24
Maximilien Noal <noal.maximilien@gmail.com>
2023-01-14 17:49:15 +0100

    VgaDac and VgaCard: Default EGA and VGA palettes
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Shared/Rgb.cs
+++ src/Spice86.Shared/Rgb.cs
@@ -4,6 +4,15 @@
 /// RGB representation of a color.
 /// </summary>
 public class Rgb {
+    public Rgb() {
+    }
+
+    public Rgb(byte r, byte g, byte b) {
+        R = r;
+        G = g;
+        B = b;
+    }
+
     public byte R { get; set; }
 
     public byte G { get; set; }
