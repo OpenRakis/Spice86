a3261a8052873d7cc758840148ce14365da6a027
Maximilien Noal <noal.maximilien@gmail.com>
2023-01-17 20:45:43 +0100

    Formatting
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/Memory/XmsHandlerFunctions.cs
+++ src/Spice86.Core/Emulator/Memory/XmsHandlerFunctions.cs
@@ -1,4 +1,5 @@
 ﻿namespace Spice86.Core.Emulator.Memory;
+
 public static class XmsHandlerFunctions {
     public const byte InstallationCheck = 0x00;
     public const byte GetCallbackAddress = 0x10;
