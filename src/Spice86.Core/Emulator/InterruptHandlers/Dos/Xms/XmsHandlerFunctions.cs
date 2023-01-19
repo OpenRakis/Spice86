ea683c8849c959c8dd89722b9a72cbecf762fdd3
Maximilien Noal <noal.maximilien@gmail.com>
2023-01-19 08:46:19 +0100

    Removed XMS
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/InterruptHandlers/Dos/Xms/XmsHandlerFunctions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Spice86.Core.Emulator.InterruptHandlers.Dos.Xms;
-
-public static class XmsHandlerFunctions {
-    public const byte InstallationCheck = 0x00;
-    public const byte GetCallbackAddress = 0x10;
-}
