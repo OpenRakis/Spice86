0752edcbef811edf5ba127c2c34f4500b1ef44be
Maximilien Noal <noal.maximilien@gmail.com>
2023-01-21 14:50:36 +0100

    Bios: CopyXMS method
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/InterruptHandlers/Bios/SystemBiosInt15Handler.cs
+++ src/Spice86.Core/Emulator/InterruptHandlers/Bios/SystemBiosInt15Handler.cs
@@ -3,12 +3,14 @@
 using Spice86.Core.Emulator.InterruptHandlers;
 using Spice86.Core.Emulator.VM;
 using Spice86.Core.Emulator.Callback;
+using Spice86.Core.Emulator.Memory;
 
 public class SystemBiosInt15Handler : InterruptHandler {
     public SystemBiosInt15Handler(Machine machine) : base(machine) {
         _dispatchTable.Add(0xC0, new Callback(0xC0, Unsupported));
         _dispatchTable.Add(0xC2, new Callback(0xC2, Unsupported));
         _dispatchTable.Add(0xC4, new Callback(0xC4, Unsupported));
+        _dispatchTable.Add(0x87, new Callback(0x87, CopyExtendedMemory));
         _dispatchTable.Add(0x88, new Callback(0x88, GetExtendedMemorySize));
     }
 
@@ -26,6 +28,18 @@ public class SystemBiosInt15Handler : InterruptHandler {
     public void GetExtendedMemorySize() {
         //We've got no extended memory (yet)
         _state.AX = 0;
+    }
+
+    public void CopyExtendedMemory() {
+        bool enabled = _memory.IsA20Enabled;
+        _machine.Memory.EnableOrDisableA20Gate(true);
+        uint bytes = _state.ECX;
+        uint data = _state.ESI;
+        long source = _memory.UInt32[data + 0x12 ] & 0x00FFFFFF + _memory.UInt8[data + 0x16] << 24;
+        long dest = _memory.UInt32[data + 0x1A] & 0x00FFFFFF + _memory.UInt8[data + 0x1E] << 24;
+        _state.EAX = (_state.EAX & 0xFFFF) | (_state.EAX & 0xFFFF0000);
+        _memory.MemCopy((uint)source, (uint)dest, bytes);
+        _memory.EnableOrDisableA20Gate(enabled);
     }
 
     private void Unsupported() {
--- src/Spice86.Core/Emulator/InterruptHandlers/Dos/Xms/XmsVirtualDevice.cs
+++ src/Spice86.Core/Emulator/InterruptHandlers/Dos/Xms/XmsVirtualDevice.cs
@@ -7,7 +7,7 @@ public class XmsVirtualDevice : DefaultIOPortHandler {
     public XmsVirtualDevice(Machine machine, Configuration configuration) : base(machine, configuration) {
     }
 
-    public override byte ReadByte(int port) => _machine.Memory.EnableA20 ? (byte)0x02 : (byte)0x00;
+    public override byte ReadByte(int port) => _machine.Memory.IsA20Enabled ? (byte)0x02 : (byte)0x00;
 
-    public override void WriteByte(int port, byte value) => _machine.Memory.EnableA20 = (value & 0x02) != 0;
+    public override void WriteByte(int port, byte value) => _machine.Memory.IsA20Enabled = (value & 0x02) != 0;
 }
