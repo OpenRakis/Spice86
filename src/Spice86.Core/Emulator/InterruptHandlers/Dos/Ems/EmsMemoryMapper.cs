98a8634c8e48a3937f5246593b05b538ee301da1
Maximilien Noal <noal.maximilien@gmail.com>
2023-01-26 19:26:59 +0100

    EMS: Moved EMS Memory out of main memory
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/EmsMemoryMapper.cs
+++ src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/EmsMemoryMapper.cs
@@ -3,6 +3,9 @@ namespace Spice86.Core.Emulator.InterruptHandlers.Dos.Ems;
 using Spice86.Core.Emulator.Memory;
 using Spice86.Core.Emulator.ReverseEngineer;
 
+/// <summary>
+/// Represents the current EMS page mapped in main memory
+/// </summary>
 public class EmsMemoryMapper : MemoryBasedDataStructureWithBaseAddress
 {
     public EmsMemoryMapper(Memory memory, uint baseAddress) : base(memory, baseAddress) {
