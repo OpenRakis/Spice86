c694a7c9c3b3dacdd52e8fa21be5d81e42aceec9
Maximilien Noal <noal.maximilien@gmail.com>
2023-02-01 21:36:55 +0100

    EMS: on write to Page Frame, write it in main memory too
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/Memory/MainMemory.cs
+++ src/Spice86.Core/Emulator/Memory/MainMemory.cs
@@ -22,23 +22,17 @@ public class MainMemory : Memory {
     }
     
     public override void SetUint32(uint address, uint value) {
-        if (_machine.Ems?.TryWriteMappedPageData(address, value) is true) {
-            return;
-        }
+        _machine.Ems?.TryWriteMappedPageData(address, value);
         base.SetUint32(address, value);
     }
     
     public override void SetUint16(uint address, ushort value) {
-        if (_machine.Ems?.TryWriteMappedPageData(address, value) is true) {
-            return;
-        }
+        _machine.Ems?.TryWriteMappedPageData(address, value);
         base.SetUint16(address, value);
     }
 
     public override void SetUint8(uint address, byte value) {
-        if (_machine.Ems?.TryWriteMappedPageData(address, value) is true) {
-            return;
-        }
+        _machine.Ems?.TryWriteMappedPageData(address, value);
         base.SetUint8(address, value);
     }
 
