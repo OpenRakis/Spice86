c7901d58add57cfc003a16bcc6c5df0f98ccf554
Maximilien Noal <noal.maximilien@gmail.com>
2023-02-15 21:59:43 +0100

    EMS: SubFunctions const in EMS class
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/EmsSubFunctions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Spice86.Core.Emulator.InterruptHandlers.Dos.Ems;
-
-internal static class EmsSubFunctions {
-    public const byte MapUnmapPages = 0x00;
-    public const byte HandleNameGet = 0x00;
-    public const byte HandleNameSet = 0x01;
-    public const byte GetHardwareInformationUnallocatedRawPages = 0x01;
-    public const byte MoveExchangeMove = 0x00;
-}
--- src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/ExpandedMemoryManager.cs
+++ src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/ExpandedMemoryManager.cs
@@ -20,6 +20,14 @@ using System.Linq;
 /// Provides DOS applications with EMS memory.
 /// </summary>
 public sealed class ExpandedMemoryManager : InterruptHandler {
+    public static class EmsSubFunctions {
+        public const byte MapUnmapPages = 0x00;
+        public const byte HandleNameGet = 0x00;
+        public const byte HandleNameSet = 0x01;
+        public const byte GetHardwareInformationUnallocatedRawPages = 0x01;
+        public const byte MoveExchangeMove = 0x00;
+    }
+
     /// <summary>
     /// Size of each EMS page in bytes.
     /// </summary>
