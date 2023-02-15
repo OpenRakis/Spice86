68a2be00147c062755e07f7c40f0436b0f72c472
Maximilien Noal <noal.maximilien@gmail.com>
2023-02-15 22:16:54 +0100

    EMS: Moved EMS Memory to EmsCard
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ src/Spice86.Core/Emulator/Devices/Memory/EmsCard.cs
@@ -0,0 +1,17 @@
+namespace Spice86.Core.Emulator.Devices.Memory; 
+
+using Spice86.Core.Emulator.IOPorts;
+using Spice86.Core.Emulator.Memory;
+using Spice86.Core.Emulator.VM;
+
+/// <summary>
+/// Basic implementation of an EMS Memory add-on card (the RAM 3000 Deluxe).
+/// </summary>
+public class EmsCard : DefaultIOPortHandler {
+    public EmsCard(Machine machine, Configuration configuration) : base(machine, configuration)
+    {
+    }
+    
+    public Memory ExpandedMemory { get; init; } = new(32 * 1024);
+
+}
