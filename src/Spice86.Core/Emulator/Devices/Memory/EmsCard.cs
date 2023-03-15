e846a87d13f190fe1f952ac1ce6820d09f65fe37
Maximilien Noal <noal.maximilien@gmail.com>
2023-03-15 23:24:50 +0100

    Replacing Aeon EMS with DOSBox EMS
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/Devices/Memory/EmsCard.cs
+++ src/Spice86.Core/Emulator/Devices/Memory/EmsCard.cs
@@ -1,8 +1,6 @@
-using Spice86.Core.Emulator.InterruptHandlers.Dos.Ems;
-
-using System.Linq;
-
 namespace Spice86.Core.Emulator.Devices.Memory; 
+
+using Spice86.Core.Emulator.InterruptHandlers.Dos.Ems;
 
 using Spice86.Core.Emulator.IOPorts;
 using Spice86.Core.Emulator.Memory;
@@ -12,30 +10,43 @@ using Spice86.Core.Emulator.VM;
 /// Basic implementation of an EMS Memory add-on card
 /// </summary>
 public class EmsCard : DefaultIOPortHandler {
+    public const int EmmMaxHandles = 200;
+    public const int MemorySizeInMb = 6;
     public EmsCard(Machine machine, Configuration configuration) : base(machine, configuration)
     {
-        for (int i = 0; i < 4096; i++) {
-            _memoryHandles.Add(i, new());
+        for (int i = 0; i < EmmHandles.Length; i++) {
+            EmmHandles[i] = new();
         }
-        _memoryHandles[0] = new EmsHandle(Enumerable.Range(0, 24).Select(i => (ushort)i));
 
+        for (int i = 0; i < EmmMappings.Length; i++) {
+            EmmMappings[i] = new();
+        }
+
+        for (int i = 0; i < EmmSegmentMappings.Length; i++) {
+            EmmSegmentMappings[i] = new();
+        }
+
+        _memory = new(MemorySizeInMb);
     }
+
+    private readonly MemoryBlock _memory;
+
+    public EmmMapping[] EmmSegmentMappings { get; private set; } = new EmmMapping[0x40];
+
+    public EmmMapping[] EmmMappings { get; private set; } = new EmmMapping[EmsHandle.EmmMaxPhysicalPages];
     
-    private readonly SortedList<int, EmsHandle> _memoryHandles = new();
-
-    public SortedList<int, EmsHandle> MemoryHandles => _memoryHandles;
-
-
+    public EmsHandle[] EmmHandles { get; private set; } = new EmsHandle[EmmMaxHandles];
+    
     public const ushort XmsStart = 0x110;
 
-    public int TotalPages => _memoryHandles.Count;
+    public int TotalPages => _memory.Pages;
 
-    public ushort FreeMemory {
+    public ushort FreeMemoryTotal {
         get {
             ushort free = 0;
             ushort index = XmsStart;
             while (index < TotalPages) {
-                if (_memoryHandles[index].PagesAllocated == 0) {
+                if (_memory.MemoryHandles[index] == 0) {
                     free++;
                 }
 
@@ -47,7 +58,7 @@ public class EmsCard : DefaultIOPortHandler {
     
     public ushort FreePages {
         get {
-            ushort count = (ushort)(FreeMemory / 4);
+            ushort count = (ushort)(FreeMemoryTotal / 4);
             if (count > 0x7FFF) {
                 return 0x7FFF;
             }
@@ -56,6 +67,9 @@ public class EmsCard : DefaultIOPortHandler {
         }
     }
     
-    public Memory ExpandedMemory { get; init; } = new(6 * 1024);
+    public Memory ExpandedMemory { get; init; } = new(MemorySizeInMb * 1024);
 
+    public int AllocatePages(ushort pages, bool sequence) {
+        throw new NotImplementedException();
+    }
 }
