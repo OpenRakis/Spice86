97b7d3d66e7aa4f9cc8049f2b556fdb0fdcc206b
Maximilien Noal <noal.maximilien@gmail.com>
2023-03-17 13:53:42 +0100

    Joystick: return no joystick installed
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/Devices/Input/Joystick/Joystick.cs
+++ src/Spice86.Core/Emulator/Devices/Input/Joystick/Joystick.cs
@@ -1,6 +1,5 @@
 ﻿namespace Spice86.Core.Emulator.Devices.Input.Joystick;
 
-using Spice86.Core.Emulator;
 using Spice86.Core.Emulator.IOPorts;
 using Spice86.Core.Emulator.VM;
 using Spice86.Shared.Interfaces;
@@ -11,10 +10,30 @@ using Spice86.Shared.Interfaces;
 public class Joystick : DefaultIOPortHandler {
     private const int JoystickPositionAndStatus = 0x201;
 
+    private byte _joystickPositionAndStatusValue = 0xFF;
+
     public Joystick(Machine machine, Configuration configuration, ILoggerService loggerService) : base(machine, configuration, loggerService) {
     }
 
     public override void InitPortHandlers(IOPortDispatcher ioPortDispatcher) {
         ioPortDispatcher.AddIOPortHandler(JoystickPositionAndStatus, this);
+    }
+
+    public override byte ReadByte(int port) {
+        return port switch {
+            JoystickPositionAndStatus => _joystickPositionAndStatusValue,
+            _ => base.ReadByte(port),
+        };
+    }
+    
+    public override void WriteByte(int port, byte value) {
+        switch (port) {
+            case JoystickPositionAndStatus:
+                _joystickPositionAndStatusValue = value;
+                break;
+            default:
+                base.WriteByte(port, value);
+                break;
+        }
     }
 }
