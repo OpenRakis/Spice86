1333b4ef1d6bb5a28ec7999009ca14a63e903306
Maximilien Noal <noal.maximilien@gmail.com>
2023-03-18 20:55:25 +0100

    UI: Debug Window (WIP)
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ src/Spice86/ViewModels/PaletteViewModel.cs
@@ -0,0 +1,47 @@
+namespace Spice86.ViewModels;
+
+using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Threading;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+using Spice86.Core.Emulator.Devices.Video;
+using Spice86.Core.Emulator.VM;
+
+public partial class PaletteViewModel : ObservableObject {
+    private readonly Machine? _machine;
+    private DispatcherTimer _timer;
+
+    public PaletteViewModel() {
+        if (!Design.IsDesignMode) {
+            throw new InvalidOperationException("This constructor is not for runtime usage");
+        }
+        _timer = new DispatcherTimer();
+    }
+
+    
+    public PaletteViewModel(Machine? machine) {
+        _machine = machine;
+        _timer = new DispatcherTimer(TimeSpan.FromSeconds(1.0 / 30.0), DispatcherPriority.Normal, UpdateColors);
+        _timer.Start();
+    }
+
+    [ObservableProperty]
+    private AvaloniaList<uint> _palette = new();
+
+    /// <summary>
+    /// Invoked by the timer to update the displayed colors.
+    /// </summary>
+    /// <param name="sender">Source of the event.</param>
+    /// <param name="e">Unused EventArgs instance.</param>
+    private void UpdateColors(object? sender, EventArgs e) {
+        AeonCard? videoCard = _machine?.VgaCard as AeonCard;
+        if (videoCard is null) {
+            return;
+        }
+        ReadOnlySpan<uint> palette = videoCard.Dac.Palette;
+        Palette.Clear();
+        Palette.AddRange(palette.ToArray());
+    }
+}
