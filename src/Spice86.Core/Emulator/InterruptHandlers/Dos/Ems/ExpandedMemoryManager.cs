99c9c2a3dac68044e4de6bd94c67b430ed4450fb
Maximilien Noal <noal.maximilien@gmail.com>
2023-03-25 10:43:57 +0100

    EMS: Rearranged methods in the Dispatch table order
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/ExpandedMemoryManager.cs
+++ src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/ExpandedMemoryManager.cs
@@ -2,6 +2,7 @@
 
 using System.Numerics;
 using Spice86.Core.Emulator.Callback;
+using Spice86.Core.Emulator.Devices.Memory;
 using Spice86.Core.Emulator.Errors;
 using Spice86.Core.Emulator.InterruptHandlers;
 using Spice86.Core.Emulator.OperatingSystem;
@@ -18,11 +19,10 @@ public sealed class ExpandedMemoryManager : InterruptHandler {
 
     public const ushort EmmMaxHandles = 200;
     public override ushort? InterruptHandlerSegment => 0xF100;
+    
+    public override byte Index => 0x67;
 
-    private readonly ILoggerService _loggerService;
-
-    public ExpandedMemoryManager(Machine machine, ILoggerService loggerService) : base(machine) {
-        _loggerService = loggerService;
+    public ExpandedMemoryManager(Machine machine) : base(machine) {
         var device = new CharacterDevice(DeviceAttributes.Ioctl, EmsIdentifier);
         machine.Dos.AddDevice(device, InterruptHandlerSegment, 0x0000);
         FillDispatchTable();
@@ -33,20 +33,117 @@ public sealed class ExpandedMemoryManager : InterruptHandler {
         _dispatchTable.Add(0x41, new Callback(0x41, GetPageFrameSegment));
         _dispatchTable.Add(0x42, new Callback(0x42, GetNumberOfPages));
         _dispatchTable.Add(0x43, new Callback(0x43, GetHandleAndAllocatePages));
-        _dispatchTable.Add(0x44, new Callback(0x44, MapExpandedMeoryPage));
+        _dispatchTable.Add(0x44, new Callback(0x44, MapExpandedMemoryPage));
         _dispatchTable.Add(0x45, new Callback(0x45, ReleaseHandleAndFreePages));
         _dispatchTable.Add(0x46, new Callback(0x46, GetEmmVersion));
         _dispatchTable.Add(0x47, new Callback(0x47, SavePageMap));
         _dispatchTable.Add(0x48, new Callback(0x48, RestorePageMap));
         _dispatchTable.Add(0x4B, new Callback(0x4B, GetHandleCount));
         _dispatchTable.Add(0x4C, new Callback(0x4C, GetPagesForOneHandle));
+        _dispatchTable.Add(0x4D, new Callback(0x4D, GetPageForAllHandles));
+        _dispatchTable.Add(0x4E, new Callback(0x4E, SaveOrRestorePageMap));
+        _dispatchTable.Add(0x4F, new Callback(0x4F, SaveOrRestorePartialPageMap));
         _dispatchTable.Add(0x50, new Callback(0x50, MapOrUnmapMultiplePageMap));
         _dispatchTable.Add(0x51, new Callback(0x51, ReallocatePages));
         _dispatchTable.Add(0x53, new Callback(0x53, SetGetHandleName));
+        _dispatchTable.Add(0x54, new Callback(0x54, HandleFunctions));
         _dispatchTable.Add(0x57, new Callback(0x57, MemoryRegion));
+        _dispatchTable.Add(0x58, new Callback(0x58, GetMappablePhysicalArrayAddressArray));
         _dispatchTable.Add(0x59, new Callback(0x59, GetHardwareInformation));
+        _dispatchTable.Add(0x5A, new Callback(0x5A, AllocateStandardRawPages));
+    }
+    
+    public void GetStatus() {
+        // Return good status in AH.
+        _state.AH = EmsErrors.EmmNoError;
+    }
+    
+    public void GetPageFrameSegment() {
+        // Return page frame segment in BX.
+        _state.BX = PageFrameSegment;
+        // Set good status.
+        _state.AH = EmsErrors.EmmNoError;
+    }
+    
+    public void GetNumberOfPages() {
+        // Return total number of pages in DX.
+        _state.DX = (ushort) (_machine.EmsCard.TotalPages / 4);
+        // Return number of pages available in BX.
+        _state.BX = _machine.EmsCard.FreePages;
+        // Set good status.
+        _state.AH = EmsErrors.EmmNoError;
+    }
+    
+    /// <summary>
+    /// Allocates pages for a new handle.
+    /// </summary>
+    public void GetHandleAndAllocatePages() {
+        ushort handles = _state.DX;
+        _state.AX = EmmAllocateMemory(_state.BX, ref handles, false);
+        _state.DX = handles;
+    }
+    
+    /// <summary>
+    /// Maps or unmaps a physical page.
+    /// </summary>
+    public void MapExpandedMemoryPage() {
+        int physicalPage = _state.AL;
+        
+    }
+    
+    /// <summary>
+    /// Deallocates a handle and all of its pages.
+    /// </summary>
+    public void ReleaseHandleAndFreePages() {
+        int handle = _state.DX;
     }
 
+    public void GetEmmVersion() {
+        // Return EMS version 4.0.
+        _state.AL = 0x40;
+        // Return good status.
+        _state.AH = 0;
+    }
+    
+    /// <summary>
+    /// Saves the current state of page map registers for a handle.
+    /// </summary>
+    public void SavePageMap() {
+    }
+    
+    /// <summary>
+    /// Restores the state of page map registers for a handle.
+    /// </summary>
+    public void RestorePageMap() {
+    }
+    
+    public void GetHandleCount() {
+        // Return the number of EMM handles (plus 1 for the OS handle).
+        _state.BX = (ushort)(_machine.EmsCard.EmmHandles.Length + 1);
+        // Return good status.
+        _state.AH = EmsErrors.EmmNoError;
+    }
+    
+    /// <summary>
+    /// Gets the number of pages allocated to a handle.
+    /// </summary>
+    public void GetPagesForOneHandle() {
+        int handleIndex = _state.DX;
+    }
+    
+    private void GetPageForAllHandles() {
+        throw new NotImplementedException();
+    }
+    
+    private void SaveOrRestorePageMap() {
+        throw new NotImplementedException();
+    }
+    
+    private void SaveOrRestorePartialPageMap() {
+        throw new NotImplementedException();
+    }
+
+    
     public void MapOrUnmapMultiplePageMap() {
         switch (_state.AL) {
             case EmsSubFunctions.MapUnmapPages:
@@ -57,60 +154,11 @@ public sealed class ExpandedMemoryManager : InterruptHandler {
         }
     }
 
-    public void GetPageFrameSegment() {
-        // Return page frame segment in BX.
-        _state.BX = PageFrameSegment;
-        // Set good status.
-        _state.AH = EmsErrors.EmmNoError;
-    }
-
-    public void GetNumberOfPages() {
-        // Return total number of pages in DX.
-        _state.DX = (ushort) (_machine.EmsCard.TotalPages / 4);
-        // Return number of pages available in BX.
-        _state.BX = _machine.EmsCard.FreePages;
-        // Set good status.
-        _state.AH = EmsErrors.EmmNoError;
-    }
-
-    public void GetEmmVersion() {
-        // Return EMS version 4.0.
-        _state.AL = 0x40;
-        // Return good status.
-        _state.AH = 0;
-    }
-
-    public void GetHardwareInformation() {
-        switch (_state.AL) {
-        case EmsSubFunctions.GetHardwareInformationUnallocatedRawPages:
-            // Return number of pages available in BX.
-            _state.BX = _machine.EmsCard.FreePages;
-            // Return total number of pages in DX.
-            _state.DX = (ushort) _machine.EmsCard.TotalPages;
-            // Set good status.
-            _state.AH = EmsErrors.EmmNoError;
-            break;
-
-        default:
-            throw new UnrecoverableException();
-        }
-    }
-
-    public void GetHandleCount() {
-        // Return the number of EMM handles (plus 1 for the OS handle).
-        _state.BX = (ushort)(_machine.EmsCard.EmmHandles.Length + 1);
-        // Return good status.
-        _state.AH = EmsErrors.EmmNoError;
-    }
-
-    public void AdvanceMap() {
-        switch (_state.AL) {
-            case EmsSubFunctions.MapUnmapPages:
-                MapUnmapMultiplePages();
-                break;
-            default:
-                throw new UnrecoverableException();
-        }
+    /// <summary>
+    /// Reallocates pages for a handle.
+    /// </summary>
+    public void ReallocatePages() {
+        
     }
 
     public void SetGetHandleName() {
@@ -128,6 +176,10 @@ public sealed class ExpandedMemoryManager : InterruptHandler {
         }
     }
 
+    private void HandleFunctions() {
+        throw new NotImplementedException();
+    }
+
     public void MemoryRegion() {
         switch (_state.AL) {
             case EmsSubFunctions.MoveExchangeMove:
@@ -139,26 +191,33 @@ public sealed class ExpandedMemoryManager : InterruptHandler {
         }
     }
 
+    private void GetMappablePhysicalArrayAddressArray() {
+        throw new NotImplementedException();
+    }
+    
+    public void GetHardwareInformation() {
+        switch (_state.AL) {
+        case EmsSubFunctions.GetHardwareInformationUnallocatedRawPages:
+            // Return number of pages available in BX.
+            _state.BX = _machine.EmsCard.FreePages;
+            // Return total number of pages in DX.
+            _state.DX = (ushort) _machine.EmsCard.TotalPages;
+            // Set good status.
+            _state.AH = EmsErrors.EmmNoError;
+            break;
 
-    public override byte Index => 0x67;
-
-    public void GetStatus() {
-        // Return good status in AH.
-        _state.AH = 0;
+        default:
+            throw new UnrecoverableException();
+        }
     }
 
+    private void AllocateStandardRawPages() {
+        throw new NotImplementedException();
+    }
+    
     public override void Run() {
         byte operation = _state.AH;
         Run(operation);
-    }
-
-    /// <summary>
-    /// Allocates pages for a new handle.
-    /// </summary>
-    public void GetHandleAndAllocatePages() {
-        ushort handles = _state.DX;
-        _state.AX = EmmAllocateMemory(_state.BX, ref handles, false);
-        _state.DX = handles;
     }
 
     public ushort EmmAllocateMemory(ushort pages, ref ushort dhandle, bool canAllocateZeroPages) {
@@ -195,34 +254,6 @@ public sealed class ExpandedMemoryManager : InterruptHandler {
     }
 
     /// <summary>
-    /// Reallocates pages for a handle.
-    /// </summary>
-    public void ReallocatePages() {
-        
-    }
-
-    /// <summary>
-    /// Deallocates a handle and all of its pages.
-    /// </summary>
-    public void ReleaseHandleAndFreePages() {
-        int handle = _state.DX;
-    }
-
-    /// <summary>
-    /// Maps or unmaps a physical page.
-    /// </summary>
-    public void MapExpandedMeoryPage() {
-        int physicalPage = _state.AL;
-    }
-
-    /// <summary>
-    /// Gets the number of pages allocated to a handle.
-    /// </summary>
-    public void GetPagesForOneHandle() {
-        int handleIndex = _state.DX;
-    }
-
-    /// <summary>
     /// Gets the name of a handle.
     /// </summary>
     public void GetHandleName() {
@@ -238,17 +269,5 @@ public sealed class ExpandedMemoryManager : InterruptHandler {
     /// Maps or unmaps multiple pages.
     /// </summary>
     public void MapUnmapMultiplePages() {
-    }
-    
-    /// <summary>
-    /// Saves the current state of page map registers for a handle.
-    /// </summary>
-    public void SavePageMap() {
-    }
-    
-    /// <summary>
-    /// Restores the state of page map registers for a handle.
-    /// </summary>
-    public void RestorePageMap() {
     }
 }
