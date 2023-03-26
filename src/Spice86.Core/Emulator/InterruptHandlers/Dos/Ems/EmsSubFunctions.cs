41ba5230b72082679b31a2cc68da97486c0c51bf
Maximilien Noal <noal.maximilien@gmail.com>
2023-03-26 18:51:33 +0200

    Ems: Removed the last bits of Aeon code
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/EmsSubFunctions.cs
+++ src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/EmsSubFunctions.cs
@@ -1,7 +1,6 @@
 namespace Spice86.Core.Emulator.InterruptHandlers.Dos.Ems; 
 
 public static class EmsSubFunctions {
-    public const byte MapUnmapPages = 0x00;
     public const byte HandleNameGet = 0x00;
     public const byte HandleNameSet = 0x01;
     public const byte GetHardwareInformationUnallocatedRawPages = 0x01;
