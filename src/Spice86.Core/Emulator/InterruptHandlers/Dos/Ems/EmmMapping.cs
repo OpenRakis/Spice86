aa4be7ca11f330e5136f38fec73f311fd1fa4358
Maximilien Noal <noal.maximilien@gmail.com>
2023-03-27 13:56:09 +0200

    EMS: EmmMapping is a simple struct
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/EmmMapping.cs
+++ src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/EmmMapping.cs
@@ -1,32 +1,6 @@
 namespace Spice86.Core.Emulator.InterruptHandlers.Dos.Ems; 
 
-public record EmmMapping {
-    public const int Size = 4;
-    private byte[] _data { get; init; } = new byte[4];
-    
-    public ushort Handle {
-        get => (ushort) ((_data[0] & 0xFF) | ((_data[1] & 0xFF) << 8));
-        set {
-            _data[0] = (byte) (value & 0xFF);
-            _data[1] = (byte) ((value >> 8) & 0xFF);
-        }
-    }
-
-    public ushort Page {
-        get => (ushort) ((_data[2] & 0xFF) | ((_data[3] & 0xFF) << 8));
-        set {
-            _data[2] = (byte) (value & 0xFF);
-            _data[3] = (byte) ((value >> 8) & 0xFF);
-        }
-    }
-
-    public void ToHandle(int val) {
-        _data[0] = (byte)(val & 0xFF);
-        _data[1] = (byte)((val >> 8) & 0xFF);
-    }
-
-    public void ToPage(int val) {
-        _data[2] = (byte)(val & 0xFF);
-        _data[3] = (byte)((val >> 8) & 0xFF);
-    }
+public struct EmmMapping {
+    public ushort Handle { get; set; }
+    public ushort Page { get; set; }
 }
