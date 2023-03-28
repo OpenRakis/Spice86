fca5ad2d55c4b406dadb0dc66ea973c1c73aa111
Maximilien Noal <noal.maximilien@gmail.com>
2023-03-28 13:41:36 +0200

    EMS: Use existing memory bus
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/RamExtensions.cs
+++ src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/RamExtensions.cs
@@ -3,15 +3,9 @@ namespace Spice86.Core.Emulator.InterruptHandlers.Dos.Ems;
 using Spice86.Core.Emulator.Memory;
 
 internal static class RamExtensions {
-    public static void BlockCopy(this Ram memory, int destAddress, int srcAddress, int size) {
+    public static void BlockCopy(this Memory memory, int destAddress, int srcAddress, int size) {
         Span<byte> src = memory.GetSpan(srcAddress, size);
         Span<byte> dest = memory.GetSpan(destAddress, size);
-        src.CopyTo(dest);
-    }
-
-    public static void WriteWord(this Ram memory, uint address, ushort value) {
-        Span<byte> src = BitConverter.GetBytes(value).AsSpan();
-        Span<byte> dest = memory.GetSpan((int) address, src.Length);
         src.CopyTo(dest);
     }
 }
