24de6fa1c403bd9f136fb4151f6d6af4e33b4c2f
Maximilien Noal <noal.maximilien@gmail.com>
2023-03-28 23:07:01 +0200

    EMS: SaveOrRestorePageMap function
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/EmmMapping.cs
+++ src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/EmmMapping.cs
@@ -5,14 +5,4 @@ using System.Linq;
 public struct EmmMapping {
     public ushort Handle { get; set; }
     public ushort Page { get; set; }
-
-    
-    public ushort Data {
-        get {
-            Span<byte> handleBytes = BitConverter.GetBytes(Handle);
-            Span<byte> pageBytes = BitConverter.GetBytes(Page);
-            Span<byte> data = stackalloc byte[] {handleBytes[0], handleBytes[1], pageBytes[0], pageBytes[1]};
-            return BinaryPrimitives.ReadUInt16LittleEndian(data);
-        }
-    }
 }
