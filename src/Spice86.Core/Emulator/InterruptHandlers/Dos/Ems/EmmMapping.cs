ce025d955b0fede23bd139ca25fb405c150ad829
Maximilien Noal <noal.maximilien@gmail.com>
2023-04-06 17:00:43 +0200

    EMS: We emulate a board, therefore EMS 3.2
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/EmmMapping.cs
+++ src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/EmmMapping.cs
@@ -1,15 +1,13 @@
 namespace Spice86.Core.Emulator.InterruptHandlers.Dos.Ems;
 
-using System.Linq;
-
 public struct EmmMapping {
     public EmmMapping() {
         Handle = 0;
         Page = 0;
     }
 
-    public ushort Handle { get; set; }
-    public ushort Page { get; set; }
+    public ushort Handle { get; set; } = ExpandedMemoryManager.EmmNullHandle;
+    public ushort Page { get; set; } = ExpandedMemoryManager.EmmNullPage;
 
     public byte[] Data { get; set; } = new byte[ExpandedMemoryManager.EmmPageSize];
 }
