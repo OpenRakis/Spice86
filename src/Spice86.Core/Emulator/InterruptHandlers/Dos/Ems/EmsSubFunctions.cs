5e86b2befe44053a813660c6aac7be86a1cbbf7f
Maximilien Noal <noal.maximilien@gmail.com>
2023-04-22 18:27:37 +0200

    EMM: GetAllHandlePages, GetHardwareInformationArray
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/EmsSubFunctions.cs
+++ src/Spice86.Core/Emulator/InterruptHandlers/Dos/Ems/EmsSubFunctions.cs
@@ -3,7 +3,7 @@ namespace Spice86.Core.Emulator.InterruptHandlers.Dos.Ems;
 public static class EmsSubFunctions {
     public const byte HandleNameGet = 0x00;
     public const byte HandleNameSet = 0x01;
-    public const byte GetHardwareInformationUnallocatedRawPages = 0x01;
-    public const byte GetHardwareConfiguration = 0x00;
+    public const byte GetUnallocatedRawPages = 0x01;
+    public const byte GetHardwareConfigurationArray = 0x00;
     public const byte MoveExchangeMove = 0x00;
 }
