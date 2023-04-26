f3116eaa7caa6de97460b41bfa02f3df032d22a3
Maximilien Noal <noal.maximilien@gmail.com>
2023-04-26 22:23:54 +0200

    Functions dump: use records and auto-properties (#301)
    * Functions dump: use records and auto-properties
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    * RecordedDataIOHandler: use properties
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    ---------
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/Function/FunctionReturn.cs
+++ src/Spice86.Core/Emulator/Function/FunctionReturn.cs
@@ -1,45 +1,14 @@
 ﻿namespace Spice86.Core.Emulator.Function;
 
-using Spice86.Core.Emulator.Memory;
+using System;
 using Spice86.Shared;
 
-using System;
-
-public class FunctionReturn : IComparable<FunctionReturn> {
-    private readonly SegmentedAddress _instructionAddress;
-
-    private readonly CallType _returnCallType;
-
-    public FunctionReturn(CallType returnCallType, SegmentedAddress instructionAddress) {
-        _returnCallType = returnCallType;
-        _instructionAddress = instructionAddress;
-    }
-
+public record FunctionReturn(CallType ReturnCallType, SegmentedAddress Address) : IComparable<FunctionReturn> {
     public int CompareTo(FunctionReturn? other) {
-        return _instructionAddress.CompareTo(other?._instructionAddress);
+        return Address.CompareTo(other?.Address);
     }
-
-    public override bool Equals(object? obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj is not FunctionReturn other) {
-            return false;
-        }
-        return
-                _instructionAddress.Equals(other._instructionAddress)
-            && _returnCallType.Equals(other._returnCallType);
-    }
-
-    public SegmentedAddress Address => _instructionAddress;
-
-    public override int GetHashCode() {
-        return HashCode.Combine(_instructionAddress, _returnCallType);
-    }
-
-    public CallType ReturnCallType => _returnCallType;
 
     public override string ToString() {
-        return $"{_returnCallType} at {_instructionAddress}";
+        return $"{ReturnCallType} at {Address}";
     }
 }
