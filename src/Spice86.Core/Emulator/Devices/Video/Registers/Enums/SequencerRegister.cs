9e0328c3068ad9de6afb15dd8d585733def6b208
JvE <joris.vaneijden@gmail.com>
2023-05-29 15:34:52 +0200

    More documentation

--- src/Spice86.Core/Emulator/Devices/Video/Registers/Enums/SequencerRegister.cs
+++ src/Spice86.Core/Emulator/Devices/Video/Registers/Enums/SequencerRegister.cs
@@ -1,9 +1,31 @@
 namespace Spice86.Core.Emulator.Devices.Video.Registers.Enums;
 
+/// <summary>
+///     Names of the sequencer registers.
+/// </summary>
 public enum SequencerRegister {
+    /// <summary>
+    ///     Reset Register
+    /// </summary>
     Reset,
+
+    /// <summary>
+    ///     Clocking Mode Register
+    /// </summary>
     ClockingMode,
+
+    /// <summary>
+    ///     Map Mask Register
+    /// </summary>
     PlaneMask,
+
+    /// <summary>
+    ///     Character Map Select Register
+    /// </summary>
     CharacterMapSelect,
+
+    /// <summary>
+    ///     Sequencer Memory Mode Register
+    /// </summary>
     MemoryMode
 }
