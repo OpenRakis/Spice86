70acfaaae1993a77fa1a2b6c255f4b9bfd7abc98
Maximilien Noal <noal.maximilien@gmail.com>
2023-06-22 08:50:42 +0200

    doc: fixed documentation in MachineCreationOptions
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/VM/MachineCreationOptions.cs
+++ src/Spice86.Core/Emulator/VM/MachineCreationOptions.cs
@@ -7,7 +7,7 @@ using Spice86.Shared.Interfaces;
 /// <summary>
 /// Describes what the emulator machine will run, and how.
 /// </summary>
-/// <param name="ProgramExecutor">The DOS program to be executed</param>
+/// <param name="ProgramExecutor">The DOS program or BIOS to be executed</param>
 /// <param name="Gui">The GUI. Can be null in headless mode.</param>
 /// <param name="LoggerService">The logger service implementation.</param>
 /// <param name="CounterConfigurator">Timer emulation configuration.</param>
