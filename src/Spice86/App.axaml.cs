b6223258843aa379e363bc13fe3c454d1fbc8f3d
Maximilien Noal <noal.maximilien@gmail.com>
2023-07-23 00:14:34 +0200

    Cleanup
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/App.axaml.cs
+++ src/Spice86/App.axaml.cs
@@ -5,7 +5,6 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
 using Spice86.Shared.Interfaces;
-using Spice86.ViewModels;
 using Spice86.Views;
 using Spice86.Core.CLI;
 
@@ -19,7 +18,5 @@ internal partial class App : Application {
     /// </summary>
     public override void Initialize() => AvaloniaXamlLoader.Load(this);
 
-    public void SetupMainWindow(IClassicDesktopStyleApplicationLifetime desktop, Configuration configuration, ILoggerService loggerService) {
-        desktop.MainWindow = new MainWindow(desktop, configuration, loggerService);
-    }
+    public void SetupMainWindow(IClassicDesktopStyleApplicationLifetime desktop, Configuration configuration, ILoggerService loggerService) => desktop.MainWindow = new MainWindow(desktop, configuration, loggerService);
 }
