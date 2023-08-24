28e3680dc86ad95b0cdb801edf269dde9aaff0d2
Maximilien Noal <noal.maximilien@gmail.com>
2023-08-24 22:20:40 +0200

    Tried to fix the game Detroit
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ src/Spice86.Core/Emulator/Devices/Input/Keyboard/KeyboardInput.cs
@@ -0,0 +1,5 @@
+namespace Spice86.Core.Emulator.Devices.Input.Keyboard; 
+
+public class KeyboardInput {
+    
+}
---
+++ src/Spice86.Core/Emulator/OperatingSystem/Structures/KeyboardStream.cs
@@ -0,0 +1,5 @@
+namespace Spice86.Core.Emulator.OperatingSystem.Structures; 
+
+public class KeyboardStream {
+    
+}
