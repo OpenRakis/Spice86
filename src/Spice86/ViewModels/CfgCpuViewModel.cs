6dd81b821ff30f71e6954af61cecef7dcb6b9acd
Maximilien Noal <noal.maximilien@gmail.com>
2024-04-13 23:57:03 +0200

    refactor: internal debugger on CfgCpu...
    ...and visits the State, the successors for the graph, and tries to
    disasm the current instructions.
    
    Also the graph is not rendered if the emulator is not paused.
    
    And it should work with loops.
    
    Finally, it displays some meta informations about the graph.
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/ViewModels/CfgCpuViewModel.cs
+++ src/Spice86/ViewModels/CfgCpuViewModel.cs
@@ -11,43 +11,94 @@ using Spice86.Core.Emulator.CPU.CfgCpu.ControlFlowGraph;
 using Spice86.Core.Emulator.CPU.CfgCpu.Linker;
 using Spice86.Core.Emulator.InternalDebugger;
 using Spice86.Infrastructure;
+using Spice86.Interfaces;
+using Spice86.Shared.Diagnostics;
+using Spice86.Shared.Interfaces;
+
+using System.Diagnostics;
 
 public partial class CfgCpuViewModel : ViewModelBase, IInternalDebugger {
+    private readonly IPerformanceMeasurer? _performanceMeasurer = new PerformanceMeasurer();
     private ExecutionContext? ExecutionContext { get; set; }
 
     [ObservableProperty]
     private Graph? _graph;
+    
+    [ObservableProperty]
+    private long _numberOfNodes = 0;
 
-    public CfgCpuViewModel(IUIDispatcherTimerFactory dispatcherTimerFactory) {
+    [ObservableProperty]
+    private long _averageNodeTime = 0;
+
+    private readonly IPauseStatus? _pauseStatus;
+
+    public CfgCpuViewModel() {
+        if(!Design.IsDesignMode) {
+            throw new InvalidOperationException("This constructor is not for runtime usage");
+        }
+    }
+
+    public CfgCpuViewModel(IUIDispatcherTimerFactory dispatcherTimerFactory, IPerformanceMeasurer performanceMeasurer, IPauseStatus pauseStatus) {
+        _pauseStatus = pauseStatus;
+        _performanceMeasurer = performanceMeasurer;
         dispatcherTimerFactory.StartNew(TimeSpan.FromMilliseconds(400), DispatcherPriority.Normal, UpdateCurrentGraph);
     }
 
     private void UpdateCurrentGraph(object? sender, EventArgs e)
     {
-        ICfgNode? nodeRoot = ExecutionContext?.LastExecuted;
-        if (nodeRoot?.Predecessors.Count is null or 0) {
+        if (_pauseStatus?.IsPaused is false or null)
+        {
             return;
         }
+        ICfgNode? nodeRoot = ExecutionContext?.LastExecuted;
+        if (nodeRoot is null)
+        {
+            return;
+        }
+
+        NumberOfNodes = 0;
         Graph currentGraph = new();
-        // Flatten the recursive Predecessors structure into an array
-        List<ICfgNode> flattenedPredecessors = new();
-        Stack<ICfgNode> stack = new();
-        stack.Push(nodeRoot);
-        ICfgNode? previousNode = nodeRoot;
-        while (stack.Count > 0) {
-            ICfgNode node = stack.Pop();
-            flattenedPredecessors.Add(node);
-            if (previousNode != null) {
+        Queue<ICfgNode> queue = new();
+        queue.Enqueue(nodeRoot);
+        HashSet<ICfgNode> visitedNodes = new();
+        var stopwatch = Stopwatch.StartNew();
+        while (queue.Count > 0)
+        {
+            ICfgNode node = queue.Dequeue();
+            if (visitedNodes.Contains(node))
+            {
+                continue;
+            }
+            visitedNodes.Add(node);
+            stopwatch.Restart();
+            foreach (ICfgNode successor in node.Successors)
+            {
                 currentGraph.Edges.Add(new Edge(
-                    $"{node.Address} {Environment.NewLine} {node.GetType()}",
-                    $"{previousNode.Address} {Environment.NewLine} {previousNode.GetType()}"));
+                    $"{node.Address} {Environment.NewLine} {node.GetType().Name}",
+                    $"{successor.Address} {Environment.NewLine} {successor.GetType().Name}"));
+                if (!visitedNodes.Contains(successor))
+                {
+                    queue.Enqueue(successor);
+                }
             }
-            foreach (ICfgNode predecessor in node.Predecessors) {
-                stack.Push(predecessor);
+            foreach (ICfgNode predecessor in node.Predecessors)
+            {
+                currentGraph.Edges.Add(new Edge(
+                    $"{predecessor.Address} {Environment.NewLine} {predecessor.GetType().Name}",
+                    $"{node.Address} {Environment.NewLine} {node.GetType().Name}"));
+                if (!visitedNodes.Contains(predecessor))
+                {
+                    queue.Enqueue(predecessor);
+                }
             }
-            previousNode = node;
+            stopwatch.Stop();
+            _performanceMeasurer?.UpdateValue(stopwatch.ElapsedMilliseconds);
+            NumberOfNodes++;
         }
         Graph = currentGraph;
+        if (_performanceMeasurer is not null) {
+            AverageNodeTime = _performanceMeasurer.ValuePerMillisecond;
+        }
     }
 
     public void Visit<T>(T component) where T : IDebuggableComponent {
