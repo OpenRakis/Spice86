d7158436becd44457e440cf3205c470d57ef2d75
Maximilien Noal <noal.maximilien@gmail.com>
2024-06-20 13:18:56 +0200

    fix: Double validation errors (AvaloniaUI + CI)
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/App.axaml.cs
+++ src/Spice86/App.axaml.cs
@@ -1,19 +1,8 @@
 namespace Spice86;
 
 using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
-using Avalonia.Threading;
-
-using Microsoft.Extensions.DependencyInjection;
-
-using Spice86.Core.CLI;
-using Spice86.DependencyInjection;
-using Spice86.Infrastructure;
-using Spice86.Shared.Interfaces;
-using Spice86.ViewModels;
-using Spice86.Views;
 
 /// <summary>
 /// The main entry point for the Spice86 UI.
@@ -26,9 +15,10 @@ internal partial class App : Application {
     public override void Initialize() => AvaloniaXamlLoader.Load(this);
 
     public override void OnFrameworkInitializationCompleted() {
-        if (ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop) {
-            throw new PlatformNotSupportedException("Spice86 requires a desktop application lifetime.");
-        }
+        // If you use CommunityToolkit, line below is needed to remove Avalonia data validation.
+        // Without this line you will get duplicate validations from both Avalonia and CT
+        BindingPlugins.DataValidators.RemoveAt(0);
+        
         base.OnFrameworkInitializationCompleted();
     }
 }
