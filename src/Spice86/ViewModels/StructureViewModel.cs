e13579ab5b2d5c54813bf96d066385d75c9ceb2a
JvE <joris.vaneijden@gmail.com>
2024-07-09 13:15:31 +0200

    Refactor and document StructureViewModel

--- src/Spice86/ViewModels/StructureViewModel.cs
+++ src/Spice86/ViewModels/StructureViewModel.cs
@@ -1,5 +1,6 @@
 namespace Spice86.ViewModels;
 
+using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.Controls.Models.TreeDataGrid;
 using Avalonia.Media;
@@ -16,38 +17,103 @@ using Spice86.Shared.Emulator.Memory;
 using Structurizer;
 using Structurizer.Types;
 
-using System.Collections.ObjectModel;
-
-public partial class StructureViewModel : ViewModelBase {
+/// <summary>
+/// ViewModel for handling the structure view in the application. It manages the display and interaction
+/// with memory structures, including selection, filtering, and updating the view based on the selected structure.
+/// </summary>
+public sealed partial class StructureViewModel : ViewModelBase, IDisposable {
     private readonly Hydrator _hydrator;
-    private readonly StructureInformation _structureInformation;
+    private readonly IBinaryDocument _originalMemory;
+    private readonly StructureInformation? _structureInformation;
+
+    [ObservableProperty]
+    private AvaloniaList<StructType> _availableStructures;
+
+    [ObservableProperty]
+    private bool _isAddressableMemory;
 
     [ObservableProperty]
     private SegmentedAddress? _memoryAddress;
 
     [ObservableProperty]
-    private ObservableCollection<StructureMember> _structureMembers = [];
+    private StructType? _selectedStructure;
+
+    [ObservableProperty]
+    private AvaloniaList<StructureMember> _structureMembers = new() {ResetBehavior = ResetBehavior.Remove};
 
     [ObservableProperty]
     private IBinaryDocument _structureMemory;
 
-    [ObservableProperty]
-    private StructType? _selectedStructure;
-
-    [ObservableProperty]
-    private bool _isAddressableMemory;
-
-
-    private readonly IBinaryDocument _originalMemory;
-
-    public event EventHandler<AddressChangedMessage>? RequestScrollToAddress;
-
-
-    /// <inheritdoc />
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StructureViewModel" /> class.
+    /// </summary>
+    /// <param name="structureInformation">The structure information containing available structures.</param>
+    /// <param name="hydrator">The hydrator used for creating structure members from binary data.</param>
+    /// <param name="data">The binary document representing the memory to be displayed and interacted with.</param>
     public StructureViewModel(StructureInformation structureInformation, Hydrator hydrator, IBinaryDocument data) {
         _structureInformation = structureInformation;
         _hydrator = hydrator;
-        Source = new HierarchicalTreeDataGridSource<StructureMember>(_structureMembers) {
+        _structureMemory = data;
+        _originalMemory = data;
+        _availableStructures = new AvaloniaList<StructType>(structureInformation.Structs.Values);
+        Source = InitializeSource();
+    }
+
+    /// <summary>
+    /// Gets or sets the <see cref="HierarchicalTreeDataGridSource{T}" /> for the structure members.
+    /// This source is used to populate the hierarchical tree data grid in the UI, allowing for the
+    /// display and interaction with the structure members.
+    /// </summary>
+    public HierarchicalTreeDataGridSource<StructureMember> Source { get; set; }
+
+    /// <summary>
+    /// Create the text that is displayed in the textbox when a structure is selected.
+    /// </summary>
+    public AutoCompleteSelector<object>? StructItemSelector { get; } = (_, item) => ((StructType)item).Name;
+
+    /// <summary>
+    /// Defines a filter for the autocomplete functionality, filtering structures based on the search text and their size.
+    /// </summary>
+    public AutoCompleteFilterPredicate<object?> StructFilter => (search, item) => search != null
+        && item is StructType structType
+        && structType.Name.Contains(search, StringComparison.OrdinalIgnoreCase)
+        && structType.Size <= (int)StructureMemory.Length;
+
+    /// <summary>
+    /// Disposes of the resources used by this instance of <see cref="StructureViewModel" />.
+    /// </summary>
+    public void Dispose() {
+        Source.Dispose();
+    }
+
+    /// <summary>
+    /// Event that is raised to request scrolling to a specific address in the memory view.
+    /// This event can be subscribed to by UI components or other parts of the application
+    /// that are responsible for displaying the memory content. When raised, it provides
+    /// an <see cref="AddressChangedMessage" /> containing the physical address to scroll to,
+    /// allowing the subscriber to react and adjust the view accordingly.
+    /// </summary>
+    public event EventHandler<AddressChangedMessage>? RequestScrollToAddress;
+
+    /// <summary>
+    /// Handles changes to the structure information, updating the available structures and re-selecting the previously
+    /// selected structure if it still exists.
+    /// </summary>
+    /// <param name="sender">The sender of the event. Not used in this method.</param>
+    /// <param name="e">The event arguments. Not used in this method.</param>
+    public void OnStructureInformationChanged(object? sender, EventArgs e) {
+        if (_structureInformation == null) {
+            AvailableStructures.Clear();
+            SelectedStructure = null;
+
+            return;
+        }
+        AvailableStructures = new AvaloniaList<StructType>(_structureInformation.Structs.Values);
+        SelectedStructure = AvailableStructures.FirstOrDefault(structType => structType.Name.Equals(SelectedStructure?.Name));
+    }
+
+    private HierarchicalTreeDataGridSource<StructureMember> InitializeSource() {
+        return new HierarchicalTreeDataGridSource<StructureMember>(StructureMembers) {
             Columns = {
                 new HierarchicalExpanderColumn<StructureMember>(new TextColumn<StructureMember, string>("Name", structureMember => structureMember.Name), structureMember => structureMember.Members),
                 new TextColumn<StructureMember, string>("Type", x => x.Type.Type),
@@ -57,26 +123,7 @@ public partial class StructureViewModel : ViewModelBase {
                 new TemplateColumn<StructureMember>("Value", DataTemplateProvider.StructureMemberValueTemplate)
             }
         };
-        StructureMemory = data;
-        _originalMemory = data;
     }
-
-    public HierarchicalTreeDataGridSource<StructureMember> Source { get; set; }
-
-    public IEnumerable<StructType> AvailableStructures => _structureInformation.Structs.Values;
-
-    /// <summary>
-    /// Create the text that is displayed in the dropdown when a structure is selected.
-    /// </summary>
-    public AutoCompleteSelector<object>? StructItemSelector { get; } = (_, item) => ((StructType)item).Name;
-
-    /// <summary>
-    /// Filter on both the search text and the size of the structure.
-    /// </summary>
-    public AutoCompleteFilterPredicate<object?> StructFilter => (search, item) => search != null
-        && item is StructType structType
-        && structType.Name.Contains(search, StringComparison.OrdinalIgnoreCase)
-        && structType.Size <= (int)StructureMemory.Length;
 
     partial void OnStructureMemoryChanged(IBinaryDocument? value) {
         IsAddressableMemory = value is DataMemoryDocument;
@@ -93,32 +140,37 @@ public partial class StructureViewModel : ViewModelBase {
         Update();
     }
 
-    public void Update() {
+    /// <summary>
+    /// Updates the view based on the current selection and state, such as the selected structure and memory address.
+    /// It refreshes the structure members displayed according to the selected structure.
+    /// </summary>
+    private void Update() {
         StructureMembers.Clear();
-        StructType? structType = SelectedStructure;
 
-        if (structType is null) {
+        if (SelectedStructure is null) {
             StructureMemory = _originalMemory;
 
             return;
         }
 
-        uint offset = 0;
-        if (IsAddressableMemory && MemoryAddress is { } address) {
-            offset = address.ToPhysical();
-        }
+        uint offset = IsAddressableMemory && MemoryAddress is { } address
+            ? address.ToPhysical()
+            : 0;
 
-        var data = new byte[structType.Size];
+        byte[] data = new byte[SelectedStructure.Size];
         _originalMemory.ReadBytes(offset, data);
 
         int index = 0;
-        foreach (var typeDefinition in structType.Members) {
+        var members = new List<StructureMember>();
+        foreach (TypeDefinition typeDefinition in SelectedStructure.Members) {
             var slice = new ReadOnlySpan<byte>(data, index, typeDefinition.Length);
             StructureMember member = _hydrator.Hydrate(typeDefinition, slice);
-            StructureMembers.Add(member);
+            members.Add(member);
             index += typeDefinition.Length;
         }
 
-        StructureMemory = new ByteArrayBinaryDocument(data.ToArray());
+        StructureMembers.AddRange(members);
+
+        StructureMemory = new ByteArrayBinaryDocument(data);
     }
 }
