733b792e388e86a3921229a1e0cf43c6d0922ecf
Maximilien Noal <noal.maximilien@gmail.com>
2024-07-29 14:00:42 +0200

    fix: dashes in passed cmdline arguments
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/CLI/CommandLineParser.cs
+++ src/Spice86.Core/CLI/CommandLineParser.cs
@@ -9,6 +9,7 @@ using Spice86.Shared.Utils;
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 
@@ -16,12 +17,15 @@ using System.Reflection;
 public class CommandLineParser : ICommandLineParser {
     /// <inheritdoc />
     public Configuration ParseCommandLine(string[] args) {
-        ParserResult<Configuration> result = Parser.Default.ParseArguments<Configuration>(args);
+        string[] reducedArgs = ProcessArgs(args, out string exeArgs);
+
+        ParserResult<Configuration> result = Parser.Default.ParseArguments<Configuration>(reducedArgs);
         return result.MapResult(initialConfig => {
             initialConfig.Exe = ParseExePath(initialConfig.Exe);
             initialConfig.CDrive ??= Path.GetDirectoryName(initialConfig.Exe);
             initialConfig.ExpectedChecksumValue = string.IsNullOrWhiteSpace(initialConfig.ExpectedChecksum) ? Array.Empty<byte>() : ConvertUtils.HexToByteArray(initialConfig.ExpectedChecksum);
             initialConfig.OverrideSupplier = ParseFunctionInformationSupplierClassName(initialConfig);
+            initialConfig.ExeArgs = exeArgs;
             return initialConfig;
         }, error => {
             string? message = "Unparseable command line";
@@ -31,13 +35,41 @@ public class CommandLineParser : ICommandLineParser {
             throw exception;
         });
     }
+    
+    private static string[] ProcessArgs(string[] args, out string exeArgs) {
+        var processedArgs = new List<string>();
+        exeArgs = string.Empty;
+        for (int i = 0; i < args.Length; i++) {
+            if (IsExeArg(args[i]) && i + 1 <= args.Length) {
+                exeArgs = args[i + 1];
+                i++;
+            } else  {
+                processedArgs.Add(args[i]);
+            }
+        }
+        return processedArgs.ToArray();
+    }
+    
+    private static bool IsExeArg(string arg) {
+        (string ShortName, string LongName)? exeArgNames = GetCommandLineOptionName<Configuration>(nameof(Configuration.ExeArgs));
+        return string.Equals(exeArgNames.Value.ShortName, arg, StringComparison.Ordinal) ||
+               string.Equals(exeArgNames.Value.LongName, arg, StringComparison.Ordinal);
+    }
+
+    /// <summary>
+    /// Gets the command line option name for a given property name via the <see cref="OptionAttribute"/>
+    /// </summary>
+    private static (string ShortName, string LongName) GetCommandLineOptionName<T>(string propertyName) {
+        PropertyInfo? property = typeof(T).GetProperty(propertyName);
+        if (property?.GetCustomAttributes(typeof(OptionAttribute), false) is not OptionAttribute[] attribute || attribute.Length == 0) {
+            throw new ArgumentException("Invalid propertyName", nameof(propertyName));
+        }
+        return ($"-{attribute[0].ShortName}", $"--{attribute[0].LongName}");
+    }
 
     private static string? ParseExePath(string? exePath) {
         string? unixPathValue = exePath?.Replace('\\', '/');
-        if (File.Exists(exePath)) {
-            return new FileInfo(exePath).FullName;
-        }
-        return unixPathValue;
+        return File.Exists(exePath) ? new FileInfo(exePath).FullName : unixPathValue;
     }
 
     private static IOverrideSupplier? ParseFunctionInformationSupplierClassName(Configuration configuration) {
