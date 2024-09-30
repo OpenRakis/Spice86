9dc2e22091ac8eea2aa14ba84cc1e8be7b717aa0
Maximilien Noal <noal.maximilien@gmail.com>
2024-09-30 20:35:09 +0200

    chore: DOS INT21H 0x4B supporting structures
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ src/Spice86.Core/Emulator/OperatingSystem/Enums/DosReturnMode.cs
@@ -0,0 +1,13 @@
+﻿namespace Spice86.Core.Emulator.OperatingSystem.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal enum DosReturnMode : byte {
+    Exit = 0,
+    CtrlC = 1,
+    Abort = 2,
+    TerminateAndStayResident = 3
+}
