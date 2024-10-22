18e5128dbfc4370f7e2b3c047be40671954e2201
Maximilien Noal <noal.maximilien@gmail.com>
2024-10-22 22:35:48 +0200

    chore: Dos(Extended)FileControlBlock struct
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ src/Spice86.Core/Emulator/OperatingSystem/Structures/DosExtendedFileControlBlock.cs
@@ -0,0 +1,23 @@
+﻿namespace Spice86.Core.Emulator.OperatingSystem.Structures;
+using Spice86.Core.Emulator.ReverseEngineer.DataStructure.Array;
+
+public class DosExtendedFileControlBlock : DosFileControlBlock {
+    public DosExtendedFileControlBlock(DosFileControlBlock dosFileControlBlock) : base(dosFileControlBlock.ByteReaderWriter, dosFileControlBlock.BaseAddress - 7) {
+    }
+
+    public byte Signature {
+        get => UInt8[0];
+        set => UInt8[0] = value;
+    }
+
+    public UInt8Array ExtendedReserved {
+        get => GetUInt8Array(1, 5);
+    }
+
+    public const byte ExpectedSignature = 0xFF;
+
+    public byte FileAttribute {
+        get => UInt8[2];
+        set => UInt8[2] = value;
+    }
+}
