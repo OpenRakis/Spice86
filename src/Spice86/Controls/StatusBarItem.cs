9e57156872206adf208b579e0d9192522d2eed34
Maximilien Noal <noal.maximilien@gmail.com>
2024-11-09 19:02:42 +0100

    feat: Slightly better StatusBar-like control
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86/Controls/StatusBarItem.cs
+++ src/Spice86/Controls/StatusBarItem.cs
@@ -1,12 +1,14 @@
 namespace Spice86.Controls;
 
 using Avalonia.Controls;
+using Avalonia.Layout;
 
 /// <summary>
 /// Control that implements an item inside a StatusBar.
 /// </summary>
 internal sealed class StatusBarItem : ContentControl {
     static StatusBarItem() {
+        HorizontalContentAlignmentProperty.OverrideDefaultValue<StatusBarItem>(HorizontalAlignment.Stretch);
         IsTabStopProperty.OverrideDefaultValue(typeof(StatusBarItem), false);
     }
 }
