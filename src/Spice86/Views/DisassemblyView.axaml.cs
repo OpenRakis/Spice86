ce2dbb5c40e385f2b135d07b63e80ce2ee76c5d8
Maximilien Noal <noal.maximilien@gmail.com>
2024-12-01 15:16:08 +0100

    feat: DISASM context menu keyboard shortcuts

--- src/Spice86/Views/DisassemblyView.axaml.cs
+++ src/Spice86/Views/DisassemblyView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 using Spice86.ViewModels;
@@ -10,6 +11,24 @@ public partial class DisassemblyView : UserControl {
     public DisassemblyView() {
         InitializeComponent();
         FunctionComboBox.SelectionChanged += FunctionComboBox_SelectionChanged;
+        DisassemblyDataGrid.KeyUp += DisassemblyDataGrid_KeyUp;
+    }
+
+    private void DisassemblyDataGrid_KeyUp(object? sender, KeyEventArgs e) {
+        if (DataContext is DisassemblyViewModel viewModel) {
+            if(e.Key == Key.C && e.KeyModifiers == KeyModifiers.Control &&
+                viewModel.CopyLineCommand.CanExecute(null)) {
+                viewModel.CopyLineCommand.Execute(null);
+            }
+            if (e.Key == Key.F2 &&
+                viewModel.CreateExecutionBreakpointHereCommand.CanExecute(null)) {
+                viewModel.CreateExecutionBreakpointHereCommand.Execute(null);
+            }
+            if (e.Key == Key.Delete &&
+                viewModel.RemoveExecutionBreakpointHereCommand.CanExecute(null)) {
+                viewModel.RemoveExecutionBreakpointHereCommand.Execute(null);
+            }
+        }
     }
 
     private void FunctionComboBox_SelectionChanged(object? sender, SelectionChangedEventArgs e) {
