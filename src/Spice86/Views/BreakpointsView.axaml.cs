39fc1fdaa35b7546506ec0951b86272b876b70f7
Maximilien Noal <noal.maximilien@gmail.com>
2024-12-01 18:16:58 +0100

    feat: Remove breakpoint keyboard shortcut

--- src/Spice86/Views/BreakpointsView.axaml.cs
+++ src/Spice86/Views/BreakpointsView.axaml.cs
@@ -1,10 +1,23 @@
 namespace Spice86.Views;
 using Avalonia.Controls;
+using Avalonia.Input;
+
+using Spice86.ViewModels;
+
+using System;
 
 public partial class BreakpointsView : UserControl
 {
     public BreakpointsView()
     {
         InitializeComponent();
+        BreakpointsDataGrid.KeyUp += BreakpointsDataGrid_KeyUp;
+    }
+
+    private void BreakpointsDataGrid_KeyUp(object? sender, KeyEventArgs e) {
+        if (e.Key == Key.Delete && DataContext is BreakpointsViewModel viewModel &&
+            viewModel.RemoveBreakpointCommand.CanExecute(null)) {
+            viewModel.RemoveBreakpointCommand.Execute(null);
+        }
     }
 }
