7ebbb3d26d833f350a7504440910fc4b2e0cad83
Maximilien Noal <noal.maximilien@gmail.com>
2024-12-01 16:08:50 +0100

    feat: DISASM UI refresh on Key.Enter usage

--- src/Spice86/Views/DisassemblyView.axaml.cs
+++ src/Spice86/Views/DisassemblyView.axaml.cs
@@ -1,7 +1,5 @@
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
-using Avalonia.Markup.Xaml;
 
 using Spice86.ViewModels;
 
@@ -11,7 +9,35 @@ public partial class DisassemblyView : UserControl {
     public DisassemblyView() {
         InitializeComponent();
         FunctionComboBox.SelectionChanged += FunctionComboBox_SelectionChanged;
+        SegmentedStartAddressTextBox.KeyUp += SegmentedStartAddressTextBox_KeyUp;
+        LinearStartAddressTextBox.KeyUp += LinearStartAddressTextBox_KeyUp;
         DisassemblyDataGrid.KeyUp += DisassemblyDataGrid_KeyUp;
+        NumberOfInstructionsShownNumericUpDown.KeyUp += NumberOfInstructionsShownNumericUpDown_KeyUp;
+    }
+
+    private void NumberOfInstructionsShownNumericUpDown_KeyUp(object? sender, KeyEventArgs e) {
+        if (DataContext is DisassemblyViewModel viewModel && e.Key == Key.Enter &&
+            viewModel.UpdateDisassemblyCommand.CanExecute(null)) {
+            viewModel.UpdateDisassemblyCommand.Execute(null);
+        }
+    }
+
+    private void LinearStartAddressTextBox_KeyUp(object? sender, KeyEventArgs e) {
+        ExecUpdateDisassemblyCommand(e, true);
+    }
+
+    private void ExecUpdateDisassemblyCommand(KeyEventArgs e, bool isUsingLinearAddressing) {
+        if (DataContext is DisassemblyViewModel viewModel && e.Key == Key.Enter) {
+            viewModel.IsUsingLinearAddressing = isUsingLinearAddressing;
+            if (viewModel.UpdateDisassemblyCommand.CanExecute(null)) {
+                viewModel.UpdateDisassemblyCommand.Execute(null);
+            }
+        }
+    }
+
+    private void SegmentedStartAddressTextBox_KeyUp(object? sender, KeyEventArgs e) {
+        ExecUpdateDisassemblyCommand(e, false);
+
     }
 
     private void DisassemblyDataGrid_KeyUp(object? sender, KeyEventArgs e) {
