fb3f353454a2e479c01f60925e42153533bccafe
Maximilien Noal <noal.maximilien@gmail.com>
2025-03-08 16:24:48 +0100

    tests: Updated AddressStringToLinearMemoryAddresssConverterTests

--- tests/Spice86.Tests/UI/Converters/AddressStringToLinearMemoryAddresssConverterTests.cs
+++ tests/Spice86.Tests/UI/Converters/AddressStringToLinearMemoryAddresssConverterTests.cs
@@ -5,7 +5,7 @@ using FluentAssertions;
 using Spice86.Converters;
 using Spice86.Core.Emulator.CPU;
 using Spice86.Core.Emulator.Memory;
-using Spice86.Shared.Emulator.Memory;
+using Spice86.Models.Debugging;
 
 using System.Globalization;
 
@@ -13,7 +13,7 @@ using Xunit;
 
 namespace Spice86.Tests.UI.Converters;
 
-public class AddressStringToSegmentedAddressConverterTests {
+public class AddressStringToLinearMemoryAddresssConverterTests {
     private readonly AddressStringToLinearMemoryAddresssConverter _converter = new();
 
     [Fact]
@@ -30,57 +30,43 @@ public class AddressStringToSegmentedAddressConverterTests {
     }
 
     [Fact]
-    public void Convert_SegmentedAddress_ReturnsFormattedString() {
+    public void Convert_LinearMemoryAddress_ReturnsFormattedString() {
         // Arrange
-        var address = new SegmentedAddress(0x1234, 0x5678);
+        var address = new LinearMemoryAddress(0x1234);
 
         // Act
         object? result = _converter.Convert(address, typeof(string),
             null, CultureInfo.InvariantCulture);
 
         // Assert
-        Assert.Equal("1234:5678", result);
+        Assert.Equal("0x1234", result);
     }
 
     [Fact]
-    public void ConvertBack_DecimalAddress_ReturnsSegmentedAddress() {
+    public void ConvertBack_DecimalAddress_ReturnsLinearMemoryAddress() {
         // Arrange
         string address = A20Gate.EndOfHighMemoryArea.ToString(CultureInfo.InvariantCulture);
 
         // Act
-        object? result = _converter.ConvertBack(address, typeof(SegmentedAddress),
+        object? result = _converter.ConvertBack(address, typeof(LinearMemoryAddress),
             null, CultureInfo.InvariantCulture);
 
         // Assert
-        new SegmentedAddress(0xFFFF, 0xFFFF).Should().BeEquivalentTo(result);
+        new LinearMemoryAddress(A20Gate.EndOfHighMemoryArea).Should().BeEquivalentTo(result);
     }
 
     [Fact]
-    public void ConvertBack_HexadecimallAddress_ReturnsSegmentedAddress() {
+    public void ConvertBack_HexadecimallAddress_ReturnsLinearMemoryAddress() {
         // Arrange
         string address = "0xFFFF";
 
         // Act
-        object? result = _converter.ConvertBack(address, typeof(SegmentedAddress),
+        object? result = _converter.ConvertBack(address, typeof(LinearMemoryAddress),
             null, CultureInfo.InvariantCulture);
 
         // Assert
-        new SegmentedAddress(0xFFF, 0x000F).Should().BeEquivalentTo(result);
+        new LinearMemoryAddress(65535).Should().BeEquivalentTo(result);
     }
-
-    [Fact]
-    public void ConvertBack_HexadecimallAddress_WithNo0x_ReturnsSegmentedAddress() {
-        // Arrange
-        string address = "FFFF";
-
-        // Act
-        object? result = _converter.ConvertBack(address, typeof(SegmentedAddress),
-            null, CultureInfo.InvariantCulture);
-
-        // Assert
-        new SegmentedAddress(0xFFF, 0x000F).Should().BeEquivalentTo(result);
-    }
-
 
     [Fact]
     public void Convert_InvalidType_ReturnsBindingNotification() {
@@ -96,39 +82,39 @@ public class AddressStringToSegmentedAddressConverterTests {
     }
 
     [Fact]
-    public void ConvertBack_NullOrWhitespaceString_ReturnsDefaultSegmentedAddress() {
+    public void ConvertBack_NullOrWhitespaceString_ReturnsNull() {
         // Arrange
         object? value = null;
 
         // Act
-        object? result = _converter.ConvertBack(value, typeof(SegmentedAddress),
+        object? result = _converter.ConvertBack(value, typeof(LinearMemoryAddress),
             null, CultureInfo.InvariantCulture);
 
         // Assert
-        new SegmentedAddress().Should().BeEquivalentTo(result);
+        BindingNotification.Null.Should().BeEquivalentTo(result);
 
         // Arrange
         value = "   ";
 
         // Act
-        result = _converter.ConvertBack(value, typeof(SegmentedAddress),
+        result = _converter.ConvertBack(value, typeof(LinearMemoryAddress),
             null, CultureInfo.InvariantCulture);
 
         // Assert
-        new SegmentedAddress().Should().BeEquivalentTo(result);
+        BindingNotification.Null.Should().BeEquivalentTo(result);
     }
 
     [Fact]
-    public void ConvertBack_ValidString_ReturnsSegmentedAddress() {
+    public void ConvertBack_ValidString_ReturnsLinearMemoryAddress() {
         // Arrange
         object value = "1234:5678";
 
         // Act
-        object? result = _converter.ConvertBack(value, typeof(SegmentedAddress),
+        object? result = _converter.ConvertBack(value, typeof(LinearMemoryAddress),
             null, CultureInfo.InvariantCulture);
 
         // Assert
-        new SegmentedAddress(0x1234, 0x5678).Should().BeEquivalentTo(result);
+        new LinearMemoryAddress(96696).Should().BeEquivalentTo(result);
     }
 
     [Fact]
@@ -137,7 +123,7 @@ public class AddressStringToSegmentedAddressConverterTests {
         object value = "invalid";
 
         // Act
-        object? result = _converter.ConvertBack(value, typeof(SegmentedAddress),
+        object? result = _converter.ConvertBack(value, typeof(LinearMemoryAddress),
             null, CultureInfo.InvariantCulture);
 
         // Assert
@@ -145,18 +131,18 @@ public class AddressStringToSegmentedAddressConverterTests {
     }
 
     [Fact]
-    public void ConvertBack_ValidStringWithState_ReturnsSegmentedAddress() {
+    public void ConvertBack_ValidStringWithState_ReturnsLinearMemoryAddress() {
         // Arrange
         var state = new State { AX = 0x1234, BX = 0x5678 };
         _converter.State = state;
         object value = "AX:BX";
 
         // Act
-        object? result = _converter.ConvertBack(value, typeof(SegmentedAddress),
+        object? result = _converter.ConvertBack(value, typeof(LinearMemoryAddress),
             null, CultureInfo.InvariantCulture);
 
         // Assert
-        new SegmentedAddress(0x1234, 0x5678).Should().BeEquivalentTo(result);
+        new LinearMemoryAddress(96696).Should().BeEquivalentTo(result);
     }
 
     [Fact]
@@ -167,24 +153,10 @@ public class AddressStringToSegmentedAddressConverterTests {
         object value = "invalid:BX";
 
         // Act
-        object? result = _converter.ConvertBack(value, typeof(SegmentedAddress),
+        object? result = _converter.ConvertBack(value, typeof(LinearMemoryAddress),
             null, CultureInfo.InvariantCulture);
 
         // Assert
         Assert.IsType<BindingNotification>(result);
-    }
-
-    [Fact]
-    public void ConvertBack_ValidStringWithLenientMode_ReturnsDefaultSegmentedAddress() {
-        // Arrange
-        object value = "invalid";
-        object parameter = "lenientMode";
-
-        // Act
-        object? result = _converter.ConvertBack(value, typeof(SegmentedAddress),
-            parameter, CultureInfo.InvariantCulture);
-
-        // Assert
-        new SegmentedAddress(0, 0).Should().BeEquivalentTo(result);
     }
 }
