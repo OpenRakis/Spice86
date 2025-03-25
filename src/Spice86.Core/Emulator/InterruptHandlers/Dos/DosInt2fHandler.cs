OpenRakis/Ix86

XMS DOS Device registered
Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

src/Spice86.Core/Emulator/InterruptHandlers/Dos/DosInt2fHandler.cs
@@ -51,7 +51,7 @@ public class DosInt2fHandler : InterruptHandler {
                 break;
             //Get XMS Control Function Address
             case 0x10:
-                State.ES = XMM.InterruptHandlerSegment;
+                State.ES = XMM.DosDeviceSegment;
                 State.BX = 0;
                 break;
             default:

OpenRakis/Spice86

src/Spice86.Core/Emulator/InterruptHandlers/Dos/DosInt2fHandler.cs
@@ -57,7 +57,7 @@ public class DosInt2fHandler : InterruptHandler {
                 break;
             //Get XMS Control Function Address
             case 0x10:
-                State.ES = ExtendedMemoryManager.InterruptHandlerSegment;
+                State.ES = ExtendedMemoryManager.DosDeviceSegment;
                 State.BX = 0;
                 break;
             default: