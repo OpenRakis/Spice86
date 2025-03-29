c36b91fa84bd338a7da28fc0c8d88d4a0e592848
Maximilien Noal <noal.maximilien@gmail.com>
2025-03-29 10:54:00 +0100

    refactor: Remove PspGenerator and EnvironmentBlockGenerator
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ src/Spice86.Core/Emulator/LoadableFile/Dos/Exe/ComFile.cs
@@ -0,0 +1,17 @@
+﻿namespace Spice86.Core.Emulator.LoadableFile.Dos.Exe;
+
+using Spice86.Core.Emulator.Memory.ReaderWriter;
+using Spice86.Core.Emulator.ReverseEngineer.DataStructure;
+
+/// <summary>
+/// Representation of an COM file as it is stored on disk, loaded into memory.
+/// </summary>
+public class ComFile : MemoryBasedDataStructure {
+    /// <summary>
+    /// The offset part of the segment:offset address of the COM file in memory.
+    /// </summary>
+    public const ushort ComOffset = 0x100;
+
+    public ComFile(IByteReaderWriter byteReaderWriter, uint baseAddress) : base(byteReaderWriter, baseAddress) {
+    }
+}
