9afea8377e3c98257110712ec727c55c545bb15c
Maximilien Noal <noal.maximilien@gmail.com>
2025-04-11 22:09:47 +0200

    refactor: DOS devices inherit some DOS files characteristics

--- src/Spice86.Core/Emulator/OperatingSystem/Devices/VirtualDeviceBase.cs
+++ src/Spice86.Core/Emulator/OperatingSystem/Devices/VirtualDeviceBase.cs
@@ -1,18 +1,20 @@
 namespace Spice86.Core.Emulator.OperatingSystem.Devices;
 
 using Spice86.Core.Emulator.OperatingSystem.Enums;
+using Spice86.Core.Emulator.OperatingSystem.Structures;
 
 /// <summary>
 /// The abstract base class for all DOS virtual devices
 /// </summary>
-public abstract class VirtualDeviceBase : IVirtualDevice {
+public abstract class VirtualDeviceBase : VirtualFileBase, IVirtualDevice {
     /// <summary>
     /// Create a new virtual device.
     /// </summary>
     /// <param name="attributes">The device attributes</param>
     /// <param name="strategy">Optional entrypoint for the strategy routine.</param>
     /// <param name="interrupt">Optional entrypoint for the interrupt routine</param>
-    protected VirtualDeviceBase(DeviceAttributes attributes, ushort strategy = 0, ushort interrupt = 0) {
+    protected VirtualDeviceBase(DeviceAttributes attributes,
+        ushort strategy = 0, ushort interrupt = 0) {
         Attributes = attributes;
         StrategyEntryPoint = strategy;
         InterruptEntryPoint = interrupt;
@@ -32,4 +34,11 @@ public abstract class VirtualDeviceBase : IVirtualDevice {
 
     /// <inheritdoc />
     public ushort InterruptEntryPoint { get; set; }
+
+    public uint DeviceNumber { get; set; }
+
+    public abstract byte GetStatus(bool inputFlag);
+    public abstract bool TryReadFromControlChannel(uint address, ushort size, out ushort? returnCode);
+
+    public abstract bool TryWriteToControlChannel(uint address, ushort size, out ushort? returnCode);
 }
