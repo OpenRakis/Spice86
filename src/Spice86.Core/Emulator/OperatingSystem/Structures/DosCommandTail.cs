a1796976a4abb50f81e44fba4e813815627634b9
Maximilien Noal <noal.maximilien@gmail.com>
2025-04-26 15:10:58 +0200

    doc: More DosCommandTail doc

--- src/Spice86.Core/Emulator/OperatingSystem/Structures/DosCommandTail.cs
+++ src/Spice86.Core/Emulator/OperatingSystem/Structures/DosCommandTail.cs
@@ -3,14 +3,24 @@
 using Spice86.Core.Emulator.Memory.ReaderWriter;
 using Spice86.Core.Emulator.ReverseEngineer.DataStructure;
 
+/// <summary>
+/// Represents a DOS command line.
+/// </summary>
 public class DosCommandTail : MemoryBasedDataStructure {
-    public DosCommandTail(IByteReaderWriter byteReaderWriter, uint baseAddress) : base(byteReaderWriter, baseAddress) {
+    public DosCommandTail(IByteReaderWriter byteReaderWriter, uint baseAddress)
+        : base(byteReaderWriter, baseAddress) {
     }
 
+    /// <summary>
+    /// A zero-terminated string that contains the command line arguments.
+    /// </summary>
     public string Command {
         get => GetZeroTerminatedString(BaseAddress, MaxCharacterLength);
         set => SetZeroTerminatedString(BaseAddress, value, MaxCharacterLength);
     }
 
+    /// <summary>
+    /// The DOS limit for the maximum length of a command line.
+    /// </summary>
     public const int MaxCharacterLength = 128;
 }
