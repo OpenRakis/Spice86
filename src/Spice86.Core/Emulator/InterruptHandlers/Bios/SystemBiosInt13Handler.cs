3657600b51c9aaabe455bb8ac3faf56e5d7772f5
Maximilien Noal <noal.maximilien@gmail.com>
2025-05-24 13:29:52 +0200

    feat: BIOS INT13H Disk interrupt handller

---
+++ src/Spice86.Core/Emulator/InterruptHandlers/Bios/SystemBiosInt13Handler.cs
@@ -0,0 +1,69 @@
+﻿namespace Spice86.Core.Emulator.InterruptHandlers.Bios;
+
+using Serilog.Events;
+
+using Spice86.Core.Emulator.CPU;
+using Spice86.Core.Emulator.Function;
+using Spice86.Core.Emulator.Memory;
+using Spice86.Shared.Interfaces;
+
+/// <summary>
+///     INT 13h handler. BIOS disk access functions.
+/// </summary>
+public class SystemBiosInt13Handler : InterruptHandler {
+    private readonly BiosDataArea _biosDataArea;
+
+    /// <summary>
+    /// Initializes a new instance.
+    /// </summary>
+    /// <param name="memory"></param>
+    /// <param name="functionHandlerProvider">Provides current call flow handler to peek call stack.</param>
+    /// <param name="stack">The CPU stack.</param>
+    /// <param name="state">The CPU state.</param>
+    /// <param name="biosDataArea">The structure where the BIOS puts its related data in emulated memory.</param>
+    /// <param name="loggerService">The logging service implementation.</param>
+    public SystemBiosInt13Handler(
+        IMemory memory, IFunctionHandlerProvider functionHandlerProvider,
+        Stack stack, State state, BiosDataArea biosDataArea,
+        ILoggerService loggerService)
+        : base(memory, functionHandlerProvider, stack, state, loggerService) {
+        _biosDataArea = biosDataArea;
+        FillDispatchTable();
+    }
+
+    /// <inheritdoc />
+    public override byte VectorNumber => 0x13;
+
+    /// <inheritdoc />
+    public override void Run() {
+        byte operation = State.AH;
+        if (LoggerService.IsEnabled(LogEventLevel.Verbose)) {
+            LoggerService.Verbose("BIOS function: 0x{@Function:X2}", operation, State.AH);
+        }
+
+        if (!HasRunnable(operation)) {
+            if (LoggerService.IsEnabled(LogEventLevel.Error)) {
+                LoggerService.Error("BIOS DISK function not provided: {@Function}", operation);
+            }
+        }
+        Run(operation);
+    }
+
+    private void FillDispatchTable() {
+        AddAction(0x0, () => ResetDiskSystem(true));
+    }
+
+    /// <summary>
+    /// Reset Disk System.
+    /// </summary>
+    /// <remarks>
+    /// This odes nothing and always clears CF.
+    /// </remarks>
+    /// <param name="calledFromVm">Whether this was called by internal emulator code or not.</param>
+    public void ResetDiskSystem(bool calledFromVm) {
+        if (LoggerService.IsEnabled(LogEventLevel.Verbose)) {
+            LoggerService.Verbose("BIOS INT13H: Reset Disk Number 0x(DiskNumber:X2)", State.DL);
+        }
+        SetCarryFlag(false, calledFromVm);
+    }
+}
