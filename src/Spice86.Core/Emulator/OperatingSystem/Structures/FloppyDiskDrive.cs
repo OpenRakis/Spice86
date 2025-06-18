cd21c6b3a982df3fff3d20e360b66f08b31de6ac
Maximilien Noal <noal.maximilien@gmail.com>
2025-06-18 21:39:32 +0200

    refactor: A DOS Virtual Drive is NOT a BlockDevice
    It cannot provide the API of a BlockDevice, and it's not like that in DOSBox Staging

---
+++ src/Spice86.Core/Emulator/OperatingSystem/Structures/FloppyDiskDrive.cs
@@ -0,0 +1,8 @@
+﻿namespace Spice86.Core.Emulator.OperatingSystem.Structures;
+/// <summary>
+/// Represents a disk image mounted as a floppy disk drive.
+/// </summary>
+/// <remarks>Unimplemented.</remarks>
+public sealed class FloppyDiskDrive : VirtualDrive {
+    public override bool IsRemovable => true;
+}
