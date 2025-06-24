af0429e49f84e1b299f86ffd2dcfd9deefbdd379
Maximilien Noal <noal.maximilien@gmail.com>
2025-06-24 22:06:59 +0200

    refactor: SB ports consts and DspReadStatus behavior
    without breaking Dune or Krondor

--- src/Spice86.Core/Emulator/Devices/Sound/Blaster/DspPorts.cs
+++ src/Spice86.Core/Emulator/Devices/Sound/Blaster/DspPorts.cs
@@ -7,45 +7,40 @@ public static class DspPorts {
     /// <summary>
     /// The I/O port used for resetting the DSP chip.
     /// </summary>
-    public const int DspReset = 0x226;
+    public const int DspReset = 0x06;
 
     /// <summary>
-    /// The port used to set the DSP status.
+    /// The port used to write data to the DSP.
     /// </summary>
-    public const int DspWriteStatus = 0x0C;
-
-    /// <summary>
-    /// The port used to get the DSP status.
-    /// </summary>
-    public const int DspReadStatus = 0x0E;
+    public const int DspWriteData = 0x0C;
 
     /// <summary>
     /// The I/O port used for reading data from the DSP chip.
     /// </summary>
-    public const int DspReadData = 0x22A;
+    public const int DspReadData = 0x0A;
 
     /// <summary>
-    /// The I/O port used for writing data to the DSP chip.
+    /// The I/O port used to get the DSP write status.
     /// </summary>
-    public const int DspWrite = 0x22C;
+    public const int DspWriteStatus = 0x0C;
 
     /// <summary>
     /// The I/O port used for checking the status of the DSP chip's input buffer.
     /// </summary>
-    public const int DspReadBufferStatus = 0x22E;
+    public const int DspReadStatus = 0x0E;
 
     /// <summary>
     /// The I/O port used for acknowledging DMA transfers to the DSP chip.
     /// </summary>
-    public const int DspDma16Acknowledge = 0x22F;
+    public const int DspDma16Acknowledge = 0x0F;
 
     /// <summary>
     /// The I/O port used for addressing the mixer chip.
     /// </summary>
-    public const int MixerAddress = 0x224;
+    public const int MixerAddress = 0x04;
 
     /// <summary>
     /// The I/O port used for writing data to the mixer chip.
     /// </summary>
-    public const int MixerData = 0x225;
+    public const int MixerData = 0x05;
 }
