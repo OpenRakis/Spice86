adbea5ae7e35c0865df412dfddb699f81d6da2ad
Maximilien Noal <noal.maximilien@gmail.com>
2025-06-27 08:10:04 +0200

    refactor: Xms uses MemoryBasedStruct, functions codes

--- src/Spice86.Core/Emulator/InterruptHandlers/Dos/Xms/XmsInt2FFunctionsCodes.cs
+++ src/Spice86.Core/Emulator/InterruptHandlers/Dos/Xms/XmsInt2FFunctionsCodes.cs
@@ -1,6 +1,6 @@
 ﻿namespace Spice86.Core.Emulator.InterruptHandlers.Dos.Xms;
 
-public static class XmsHandlerFunctions {
+public static class XmsInt2FFunctionsCodes {
     public const byte InstallationCheck = 0x00;
     public const byte GetCallbackAddress = 0x10;
 }
