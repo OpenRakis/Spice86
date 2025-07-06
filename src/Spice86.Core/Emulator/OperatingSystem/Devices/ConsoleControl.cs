434894b8aa9bf2b7fb0d0d710c04287e66c0b0a5
Maximilien Noal <noal.maximilien@gmail.com>
2025-07-06 18:14:59 +0200

    fix: Properly control Console Device echo status

---
+++ src/Spice86.Core/Emulator/OperatingSystem/Devices/ConsoleControl.cs
@@ -0,0 +1,12 @@
+﻿namespace Spice86.Core.Emulator.OperatingSystem.Devices;
+
+/// <summary>
+/// Provides control over the internal <see cref="ConsoleDevice"/> behavior, when standard output/input isn't redirected.
+/// </summary>
+public sealed class ConsoleControl {
+    public bool InternalOutput { get; set; }
+
+    public bool Echo { get; set; }
+
+    public bool DirectOutput { get; set; }
+}
