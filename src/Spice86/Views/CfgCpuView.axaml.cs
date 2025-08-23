e4b1d65c3bbbf0f6c8193b00e56c082a835ed860
Maximilien Noal <noal.maximilien@gmail.com>
2025-08-23 11:04:35 +0200

    feat: Cfg Graph search and more details
    Updated CfgCpuViewModel to support search functionality with new properties and commands for navigating nodes.

--- src/Spice86/Views/CfgCpuView.axaml.cs
+++ src/Spice86/Views/CfgCpuView.axaml.cs
@@ -1,7 +1,23 @@
 namespace Spice86.Views;
 
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Spice86.ViewModels;
 
-internal sealed partial class CfgCpuView : UserControl {
-    public CfgCpuView() => InitializeComponent();
+public partial class CfgCpuView : UserControl
+{
+    public CfgCpuView()
+    {
+        InitializeComponent();
+    }
+    
+    private void OnAutoCompleteKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter && DataContext is CfgCpuViewModel viewModel)
+        {
+            viewModel.NavigateToSelectedNodeCommand.Execute(null);
+            e.Handled = true;
+        }
+    }
 }
