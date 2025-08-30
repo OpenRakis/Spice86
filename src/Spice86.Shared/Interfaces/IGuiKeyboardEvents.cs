f63db38315944c1922531df6ef27434a78d088e3
Maximilien Noal <noal.maximilien@gmail.com>
2025-08-30 16:33:12 +0200

    refactor: interfaces for keyboard and mouse
    Updated the PS2Keyboard, Mouse, VgaCard, and MouseDriver
    classes to use more specific interfaces for handling keyboard,
    mouse, and video events. Removed the IGui interface and
    introduced IGuiKeyboardEvents, IGuiMouseEvents, and
    IGuiVideoPresentation for better separation of concerns.
    The HeadlessGui class now implements these new interfaces,
    and the Spice86DependencyInjection and MainWindowViewModel
    classes were modified accordingly to support the changes.

---
+++ src/Spice86.Shared/Interfaces/IGuiKeyboardEvents.cs
@@ -0,0 +1,19 @@
+﻿namespace Spice86.Shared.Interfaces;
+using Spice86.Shared.Emulator.Keyboard;
+
+using System;
+
+/// <summary>
+/// Events fired by the emulator UI when a keyboard key is pressed or released.
+/// </summary>
+public interface IGuiKeyboardEvents {
+    /// <summary>
+    /// Fired when a keyboard key is released.
+    /// </summary>
+    public event EventHandler<KeyboardEventArgs>? KeyUp;
+
+    /// <summary>
+    /// Fired when a keyboard key is pressed.
+    /// </summary>
+    public event EventHandler<KeyboardEventArgs>? KeyDown;
+}
