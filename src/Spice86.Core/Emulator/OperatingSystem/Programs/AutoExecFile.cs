4565361d9d6f125d502134a4dbf03695de24cd23
Maximilien Noal <noal.maximilien@gmail.com>
2025-10-05 11:04:36 +0200

    feat: COMMAND.COM with AUTOEXEC.BAT generation (WIP)

---
+++ src/Spice86.Core/Emulator/OperatingSystem/Programs/AutoExecFile.cs
@@ -0,0 +1,24 @@
+﻿namespace Spice86.Core.Emulator.OperatingSystem.Programs;
+
+using Spice86.Core.Emulator.OperatingSystem.Structures;
+
+internal class AutoExecFile : IVirtualFile {
+    private readonly Configuration _configuration;
+
+    public AutoExecFile(Configuration configuration) {
+        _configuration = configuration;
+    }
+
+    public string Name {
+        get => "AUTOEXEC.BAT";
+        set => throw new InvalidOperationException("Cannot rename a built-in operating system file");
+    }
+
+    public IEnumerable<string> GetLines() {
+        string? hostFilePath = _configuration.Exe;
+        string? arguments = _configuration.ExeArgs;
+        string? cDrivePath = _configuration.CDrive;
+        string commandLine = $"{Path.GetRelativePath(cDrivePath!, hostFilePath!)} {arguments}";
+        yield return commandLine;
+    }
+}
---
+++ src/Spice86.Core/Emulator/OperatingSystem/Programs/CommandInterpreter.cs
@@ -0,0 +1,16 @@
+﻿namespace Spice86.Core.Emulator.OperatingSystem.Programs;
+
+using Spice86.Core.Emulator.InterruptHandlers.Common.MemoryWriter;
+using Spice86.Core.Emulator.Memory.ReaderWriter;
+
+internal class CommandInterpreter : DosInternalProgram {
+    public CommandInterpreter(MemoryAsmWriter memoryAsmWriter, IByteReaderWriter byteReaderWriter, uint baseAddress)
+        : base(memoryAsmWriter, byteReaderWriter, baseAddress) {
+        
+    }
+
+    public override string Name {
+        get => "COMMAND.COM";
+        set => throw new InvalidOperationException("Cannot rename a built-in operating system file");
+    }
+}
