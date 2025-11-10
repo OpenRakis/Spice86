dc3bbf6e4cb0f431d2cd761f34f3267f9985db00
Stefan Hueg <498798+codengine@users.noreply.github.com>
2025-11-10 14:53:03 +0100

    CPUID now throws a CpuInvalidOpcodeException

--- src/Spice86.Core/Emulator/CPU/CfgCpu/ParsedInstruction/Instructions/Cpuid.cs
+++ src/Spice86.Core/Emulator/CPU/CfgCpu/ParsedInstruction/Instructions/Cpuid.cs
@@ -6,14 +6,11 @@ using Spice86.Core.Emulator.CPU.CfgCpu.InstructionExecutor;
 using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Prefix;
 using Spice86.Core.Emulator.CPU.Exceptions;
 using Spice86.Shared.Emulator.Memory;
-using Spice86.Shared.Utils;
 
 public class Cpuid(SegmentedAddress address, InstructionField<ushort> opcodeField, List<InstructionPrefix> prefixes)
     : CfgInstruction(address, opcodeField, prefixes, 1) {
     public override void Execute(InstructionExecutionHelper helper) {
-        // 80386-class CPUs do not support CPUID; raise invalid opcode like the interpreter
-        string message = $"opcode={ConvertUtils.ToHex(OpcodeField.Value)}";
-        helper.HandleCpuException(this, new CpuInvalidOpcodeException(message));
+        throw new CpuInvalidOpcodeException("Attempted to call CPUID, which is unsupported on CPUs < 486");
     }
 
     public override InstructionNode ToInstructionAst(AstBuilder builder) {
