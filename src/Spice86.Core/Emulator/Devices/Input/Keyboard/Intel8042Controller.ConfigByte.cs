472ee6f3273e4f72a2c4996e732f2735e82d5589
Maximilien Noal <noal.maximilien@gmail.com>
2025-11-18 22:16:34 +0100

    doc: more XMl doc in Intel8041Controller

--- src/Spice86.Core/Emulator/Devices/Input/Keyboard/Intel8042Controller.ConfigByte.cs
+++ src/Spice86.Core/Emulator/Devices/Input/Keyboard/Intel8042Controller.ConfigByte.cs
@@ -6,6 +6,7 @@ public partial class Intel8042Controller {
     /// <summary>
     /// Byte 0x00 of the controller memory - configuration byte
     /// </summary>
+    /// <remarks>This has been converted into a class for easier debugging and implementation.</remarks>
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public sealed class ConfigByte {
         public ConfigByte(byte initial = 0) => FromByte(initial);
@@ -13,34 +14,73 @@ public partial class Intel8042Controller {
         /// <summary>
         /// bit0: keyboard IRQ enabled (IRQ1)
         /// </summary>
+        /// <remarks>
+        /// Read when activating IRQs and by the <see cref="KeyboardCommand.ReadOutputPort"/> (0xD0) command.
+        /// Written by the <see cref="KeyboardCommand.WriteByteConfig"/> (0x60) command.
+        /// </remarks>
         public bool KbdIrqEnabled { get; set; }
         /// <summary>
         /// bit1: mouse IRQ enabled (IRQ12)
         /// </summary>
+        /// <remarks>
+        /// Read when activating IRQs and by the <see cref="KeyboardCommand.ReadOutputPort"/> (0xD0) command.
+        /// Written by the <see cref="KeyboardCommand.WriteByteConfig"/> (0x60) command.
+        /// </remarks>
         public bool MouseIrqEnabled { get; set; }
         /// <summary>
         /// bit2: self-test passed
         /// </summary>
+        /// <remarks>
+        /// Written by the <see cref="KeyboardCommand.TestController"/> (0xAA) and <see cref="KeyboardCommand.WriteByteConfig"/> (0x60) commands.
+        /// </remarks>
         public bool SelfTestPassed { get; set; }
         /// <summary>
         /// bit3: reserved
         /// </summary>
+        /// <remarks>
+        /// This bit is forced to 0 when the configuration byte is written via the <see cref="KeyboardCommand.WriteByteConfig"/> (0x60) command.
+        /// </remarks>
         public bool Reserved3 { get; set; }
         /// <summary>
         /// bit4: keyboard port disabled
         /// </summary>
+        /// <remarks>
+        /// Read when data is added to the buffer from the keyboard.
+        /// Written by the <see cref="KeyboardCommand.DisablePortKbd"/> (0xAD),
+        /// <see cref="KeyboardCommand.EnableKeyboardPort"/> (0xAE),
+        /// <see cref="KeyboardCommand.TestController"/> (0xAA),
+        /// <see cref="KeyboardCommand.TestPortKbd"/> (0xAB), and
+        /// <see cref="KeyboardCommand.WriteByteConfig"/> (0x60) commands.
+        /// Also written when data is sent to the keyboard.
+        /// </remarks>
         public bool IsKeyboardPortDisabled { get; set; }
         /// <summary>
         /// bit5: aux (mouse) port disabled
         /// </summary>
+        /// <remarks>
+        /// Read when data is added to the buffer from the auxiliary device.
+        /// Written by the <see cref="KeyboardCommand.DisablePortAux"/> (0xA7),
+        /// <see cref="KeyboardCommand.EnablePortAux"/> (0xA8),
+        /// <see cref="KeyboardCommand.TestController"/> (0xAA),
+        /// <see cref="KeyboardCommand.TestPortAux"/> (0xA9), and
+        /// <see cref="KeyboardCommand.WriteByteConfig"/> (0x60) commands.
+        /// </remarks>
         public bool IsAuxPortDisabled { get; set; }
         /// <summary>
         /// bit6: translation enabled (AT -> XT)
         /// </summary>
+        /// <remarks>
+        /// Read when keyboard data is added to the buffer to determine if scancode translation is needed.
+        /// Written by the <see cref="KeyboardCommand.TestController"/>
+        /// (0xAA) and <see cref="KeyboardCommand.WriteByteConfig"/> (0x60) commands.
+        /// </remarks>
         public bool TranslationEnabled { get; set; }
         /// <summary>
         /// bit7: reserved
         /// </summary>
+        /// <remarks>
+        /// This bit is forced to 0 when the configuration byte is written via the <see cref="KeyboardCommand.WriteByteConfig"/> (0x60) command.
+        /// </remarks>
         public bool Reserved7 { get; set; }
 
         public byte ToByte() {
