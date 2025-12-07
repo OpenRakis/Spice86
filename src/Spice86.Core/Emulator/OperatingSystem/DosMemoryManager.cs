832df5d7d19598bd220f7957477f3ecfa650da1e
Maximilien Noal <noal.maximilien@gmail.com>
2025-12-07 11:54:39 +0100

    fix: com loading behavior (#1610)
    * fix: com loading behavior
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    * fix: Also set SS register to pspSegment in COM loading branch
    
    * refactor: clearer consts with names instead of magic values
    
    * refactor: replaced comment with method
    
    * refactor: programEntryPointSeg -> programEntryPoint
    
    * docs: clarified consts
    
    * Revert "refactor: programEntryPointSeg -> programEntryPoint"
    
    This reverts commit 7cd1acf115f9025f3263e6efe3fd191da5740a9e.
    
    * refactor: clarified DosProcessManager seek comment
    
    * refactor: renamed PSP property, clarified DosProcessManager
    
    * refactor: clarified DOSProcessManager comment
    
    * refactor: reverted back to DataBlockSegment
    
    * refactor: renamed to loadImageSegment all over
    
    * refactor: simpler comment
    
    * refactor: remove now unused GetProgramEntryPoint()
    
    * refactor: add log for (most certainly) dead code in LoadExeFile
    
    * refactor: clarified special load behavior for rare EXEs
    
    ---------
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Spice86.Core/Emulator/OperatingSystem/DosMemoryManager.cs
+++ src/Spice86.Core/Emulator/OperatingSystem/DosMemoryManager.cs
@@ -35,14 +35,14 @@ public class DosMemoryManager {
         // The MCB starts 1 paragraph (16 bytes) before the 16 paragraph (256 bytes) PSP. Since
         // we're the memory manager, we're the one who needs to read the MCB, so we need to start
         // with its address by subtracting 1 paragraph from the PSP.
-        ushort startSegment = (ushort)(pspSegment - 1);
-        _start = GetDosMemoryControlBlockFromSegment(startSegment);
-        // LastFreeSegment and startSegment are both valid segments that may be allocated, so we
+        ushort loadSegment = (ushort)(pspSegment - 1);
+        _start = GetDosMemoryControlBlockFromSegment(loadSegment);
+        // LastFreeSegment and loadSegment are both valid segments that may be allocated, so we
         // need to add 1 paragraph to the result to ensure that our calculated size doesn't exclude
         // LastFreeSegment from being allocated. Some games do their own math to calculate the
         // maximum free conventional memory from the last block that was allocated rather than
         // asking the memory manager, and if we were off by one, allocation would fail.
-        ushort size = (ushort)((LastFreeSegment - startSegment) + 1);
+        ushort size = (ushort)((LastFreeSegment - loadSegment) + 1);
         // We adjusted the start address above so that it starts with the MCB, but the MCB itself
         // isn't actually useable space. We need it here in the DOS memory manager for accounting.
         // Therefore subtract the size of the MCB (1 paragraph, which is 16 bytes) from the total
