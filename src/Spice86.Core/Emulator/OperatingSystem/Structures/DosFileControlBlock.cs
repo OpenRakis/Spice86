a6899dc5b4e52e4b9f0e96a8f746ff0763518650
Maximilien Noal <noal.maximilien@gmail.com>
2026-01-13 20:38:58 +0100

    Add full DOS FCB file API support to INT 21h handler
    Implemented legacy FCB file operations (open, close, create, sequential/random read/write, find first/next, get file size, set random record, filename parsing) via new DosFcbManager. Added typed FCB and extended FCB structures for memory access. INT 21h handler now dispatches FCB functions and manages search state for Find First/Next, supporting both standard and extended FCBs.
    
    Fixes games like Civilization 1, Reunion.

---
+++ src/Spice86.Core/Emulator/OperatingSystem/Structures/DosFileControlBlock.cs
@@ -0,0 +1,276 @@
+namespace Spice86.Core.Emulator.OperatingSystem.Structures;
+
+using Spice86.Core.Emulator.Memory.ReaderWriter;
+using Spice86.Core.Emulator.ReverseEngineer.DataStructure;
+
+using System.Text;
+
+/// <summary>
+/// Represents a DOS File Control Block (FCB) in memory.
+/// The FCB is a CP/M-style data structure used for file operations in DOS.
+/// </summary>
+/// <remarks>
+/// <para>
+/// The FCB structure is 37 bytes and contains:
+/// <list type="bullet">
+///   <item>Offset 0x00 (1 byte): Drive number (0=default, 1=A, 2=B, etc.)</item>
+///   <item>Offset 0x01 (8 bytes): File name (space-padded)</item>
+///   <item>Offset 0x09 (3 bytes): File extension (space-padded)</item>
+///   <item>Offset 0x0C (2 bytes): Current block number</item>
+///   <item>Offset 0x0E (2 bytes): Record size (default 128)</item>
+///   <item>Offset 0x10 (4 bytes): File size in bytes</item>
+///   <item>Offset 0x14 (2 bytes): Date of last write (DOS date format)</item>
+///   <item>Offset 0x16 (2 bytes): Time of last write (DOS time format)</item>
+///   <item>Offset 0x18 (8 bytes): Reserved (system use)</item>
+///   <item>Offset 0x20 (1 byte): Current record number</item>
+///   <item>Offset 0x21 (4 bytes): Random record number</item>
+/// </list>
+/// </para>
+/// <para>
+/// Based on FreeDOS kernel implementation: https://github.com/FDOS/kernel/blob/master/hdr/fcb.h
+/// </para>
+/// </remarks>
+public class DosFileControlBlock : MemoryBasedDataStructure {
+    /// <summary>
+    /// Total size of an FCB structure in bytes.
+    /// </summary>
+    public const int StructureSize = 37;
+
+    /// <summary>
+    /// Default record size for FCB operations.
+    /// </summary>
+    public const ushort DefaultRecordSize = 128;
+
+    /// <summary>
+    /// Maximum file name length (8 characters).
+    /// </summary>
+    public const int FileNameSize = 8;
+
+    /// <summary>
+    /// Maximum file extension length (3 characters).
+    /// </summary>
+    public const int FileExtensionSize = 3;
+
+    // Field offsets
+    private const int DriveNumberOffset = 0x00;
+    private const int FileNameOffset = 0x01;
+    private const int FileExtensionOffset = 0x09;
+    private const int CurrentBlockOffset = 0x0C;
+    private const int RecordSizeOffset = 0x0E;
+    private const int FileSizeOffset = 0x10;
+    private const int DateOffset = 0x14;
+    private const int TimeOffset = 0x16;
+    private const int ReservedOffset = 0x18;
+    private const int CurrentRecordOffset = 0x20;
+    private const int RandomRecordOffset = 0x21;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DosFileControlBlock"/> class.
+    /// </summary>
+    /// <param name="byteReaderWriter">The memory bus for reading/writing FCB data.</param>
+    /// <param name="baseAddress">The base address of the FCB in memory.</param>
+    public DosFileControlBlock(IByteReaderWriter byteReaderWriter, uint baseAddress)
+        : base(byteReaderWriter, baseAddress) {
+    }
+
+    /// <summary>
+    /// Gets or sets the drive number.
+    /// 0 = default drive, 1 = A:, 2 = B:, etc.
+    /// </summary>
+    public byte DriveNumber {
+        get => UInt8[DriveNumberOffset];
+        set => UInt8[DriveNumberOffset] = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the file name (8 characters, space-padded).
+    /// </summary>
+    public string FileName {
+        get => GetSpacePaddedString(FileNameOffset, FileNameSize);
+        set => SetSpacePaddedString(FileNameOffset, value, FileNameSize);
+    }
+
+    /// <summary>
+    /// Gets or sets the file extension (3 characters, space-padded).
+    /// </summary>
+    public string FileExtension {
+        get => GetSpacePaddedString(FileExtensionOffset, FileExtensionSize);
+        set => SetSpacePaddedString(FileExtensionOffset, value, FileExtensionSize);
+    }
+
+    /// <summary>
+    /// Gets or sets the current block number.
+    /// Each block contains 128 records.
+    /// </summary>
+    public ushort CurrentBlock {
+        get => UInt16[CurrentBlockOffset];
+        set => UInt16[CurrentBlockOffset] = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the logical record size in bytes.
+    /// Default is 128 bytes.
+    /// </summary>
+    public ushort RecordSize {
+        get => UInt16[RecordSizeOffset];
+        set => UInt16[RecordSizeOffset] = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the file size in bytes.
+    /// </summary>
+    public uint FileSize {
+        get => UInt32[FileSizeOffset];
+        set => UInt32[FileSizeOffset] = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the file date in DOS format.
+    /// </summary>
+    public ushort Date {
+        get => UInt16[DateOffset];
+        set => UInt16[DateOffset] = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the file time in DOS format.
+    /// </summary>
+    public ushort Time {
+        get => UInt16[TimeOffset];
+        set => UInt16[TimeOffset] = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the SFT (System File Table) number.
+    /// This is used internally by DOS to track the open file.
+    /// </summary>
+    public byte SftNumber {
+        get => UInt8[ReservedOffset];
+        set => UInt8[ReservedOffset] = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the high byte of device attributes.
+    /// </summary>
+    public byte AttributeHigh {
+        get => UInt8[ReservedOffset + 1];
+        set => UInt8[ReservedOffset + 1] = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the low byte of device attributes.
+    /// </summary>
+    public byte AttributeLow {
+        get => UInt8[ReservedOffset + 2];
+        set => UInt8[ReservedOffset + 2] = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the starting cluster of the file.
+    /// </summary>
+    public ushort StartCluster {
+        get => UInt16[ReservedOffset + 3];
+        set => UInt16[ReservedOffset + 3] = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the cluster of the directory entry.
+    /// </summary>
+    public ushort DirectoryCluster {
+        get => UInt16[ReservedOffset + 5];
+        set => UInt16[ReservedOffset + 5] = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the offset of the directory entry (unused).
+    /// </summary>
+    public byte DirectoryOffset {
+        get => UInt8[ReservedOffset + 7];
+        set => UInt8[ReservedOffset + 7] = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the current record number within the current block.
+    /// </summary>
+    public byte CurrentRecord {
+        get => UInt8[CurrentRecordOffset];
+        set => UInt8[CurrentRecordOffset] = value;
+    }
+
+    /// <summary>
+    /// Gets or sets the random record number for random I/O operations.
+    /// </summary>
+    public uint RandomRecord {
+        get => UInt32[RandomRecordOffset];
+        set => UInt32[RandomRecordOffset] = value;
+    }
+
+    /// <summary>
+    /// Gets the absolute record number based on current block and record.
+    /// </summary>
+    public uint AbsoluteRecord => (uint)CurrentBlock * 128 + CurrentRecord;
+
+    /// <summary>
+    /// Gets the full 8.3 file name as "FILENAME.EXT" format.
+    /// </summary>
+    public string FullFileName {
+        get {
+            string name = FileName.TrimEnd();
+            string ext = FileExtension.TrimEnd();
+            return string.IsNullOrEmpty(ext) ? name : $"{name}.{ext}";
+        }
+    }
+
+    /// <summary>
+    /// Advances to the next record, updating block if necessary.
+    /// </summary>
+    public void NextRecord() {
+        if (++CurrentRecord >= 128) {
+            CurrentRecord = 0;
+            CurrentBlock++;
+        }
+    }
+
+    /// <summary>
+    /// Sets the current block and record from a random record number.
+    /// </summary>
+    public void CalculateRecordPosition() {
+        CurrentBlock = (ushort)(RandomRecord / 128);
+        CurrentRecord = (byte)(RandomRecord % 128);
+    }
+
+    /// <summary>
+    /// Sets the random record number from current block and record.
+    /// </summary>
+    public void SetRandomFromPosition() {
+        RandomRecord = AbsoluteRecord;
+    }
+
+    /// <summary>
+    /// Gets a space-padded string from memory.
+    /// </summary>
+    /// <param name="offset">The offset from the base address.</param>
+    /// <param name="length">The fixed length of the string field.</param>
+    /// <returns>The string, including trailing spaces.</returns>
+    private string GetSpacePaddedString(int offset, int length) {
+        StringBuilder result = new();
+        for (int i = 0; i < length; i++) {
+            byte b = UInt8[(uint)offset + (uint)i];
+            result.Append((char)b);
+        }
+        return result.ToString();
+    }
+
+    /// <summary>
+    /// Sets a space-padded string in memory.
+    /// </summary>
+    /// <param name="offset">The offset from the base address.</param>
+    /// <param name="value">The string value to write.</param>
+    /// <param name="length">The fixed length of the string field.</param>
+    private void SetSpacePaddedString(int offset, string value, int length) {
+        byte[] bytes = Encoding.ASCII.GetBytes(value.PadRight(length));
+        for (int i = 0; i < length; i++) {
+            UInt8[(uint)offset + (uint)i] = bytes[i];
+        }
+    }
+}
