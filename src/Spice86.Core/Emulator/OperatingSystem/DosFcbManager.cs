8aca3457bc2631087fc83279681a2b3e0378077f
Maximilien Noal <noal.maximilien@gmail.com>
2026-01-20 12:35:30 +0100

    fixx: FindFirst prevents mem leak

--- src/Spice86.Core/Emulator/OperatingSystem/DosFcbManager.cs
+++ src/Spice86.Core/Emulator/OperatingSystem/DosFcbManager.cs
@@ -1003,6 +1003,15 @@ public class DosFcbManager {
             driveNumber = (byte)(_dosDriveManager.CurrentDriveIndex + 1);
         }
 
+        // Clean up any previous search state on this FCB before starting a new one
+        // This prevents memory leaks when FindFirst is called multiple times on the same FCB
+        uint oldSearchId = GetFcbSearchState(fcbAddress, isExtended);
+        if (oldSearchId != 0 && _fcbActiveSearches.Remove(oldSearchId)) {
+            if (_loggerService.IsEnabled(LogEventLevel.Debug)) {
+                _loggerService.Debug("FCB Find First: Cleaned up previous search state (SearchId: {Id})", oldSearchId);
+            }
+        }
+
         // Get the search folder and pattern from the FCB path
         string searchPattern = GetSearchPattern(fcb);
         string? searchFolder = GetSearchFolder(dosPath);
