4f34c41572da5cb18fa95b6f352110e5c1409ccf
Maximilien Noal <noal.maximilien@gmail.com>
2026-02-23 21:39:29 +0100

    refactor: use Spice86.Audio Nuget package

--- src/Spice86.Audio/Filters/IirFilters/Common/State/ISectionState.cs
+++
@@ -1,9 +0,0 @@
-namespace Spice86.Audio.Filters.IirFilters.Common.State;
-
-using Spice86.Audio.Filters.IirFilters.Common;
-
-public interface ISectionState {
-    void Reset();
-
-    double Process(double input, Biquad coefficients);
-}
